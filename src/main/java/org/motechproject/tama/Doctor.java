217da649e049d992b8a4ca2484ac75dca785df7a
Alok Simha <lukasimha@gmail.com>
2011-06-27 19:57:55 +0530

    #0001: generate roo.equals to bind selected gender and doctor on a patient while updating <Luka/Katta>

--- src/main/java/org/motechproject/tama/Doctor.java
+++ src/main/java/org/motechproject/tama/Doctor.java
@@ -3,12 +3,13 @@ package org.motechproject.tama;
 import org.springframework.roo.addon.entity.RooEntity;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.tostring.RooToString;
-
 import javax.validation.constraints.NotNull;
+import de.saxsys.roo.equals.addon.RooEquals;
 
 @RooJavaBean
 @RooToString
 @RooEntity
+@RooEquals
 public class Doctor {
 
     @NotNull
