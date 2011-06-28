b525e5d242c00afadd70ccd2cf22d7402c135d1d
Sushant <sushants@thoughtworks.com>
2011-06-28 13:53:58 +0530

    [Shruthi/Sushant]Finished the implementation for Functional Tests, Added HTMLUnitDriver

--- src/test/java/org/motechproject/tama/functional/tests/LoginTest.java
+++ src/test/java/org/motechproject/tama/functional/tests/LoginTest.java
@@ -1,17 +1,22 @@
 package org.motechproject.tama.functional.tests;
 
-import com.thoughtworks.selenium.SeleneseTestCase;
 import junit.framework.Assert;
-import org.junit.Test;
+import org.junit.*;
 import org.motechproject.tama.functional.pages.HomePage;
 import org.motechproject.tama.functional.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginTest{
 
-    private WebDriver webDriver = new FirefoxDriver();
+    private static WebDriver webDriver;
+
+    @BeforeClass
+    public static void setUp(){
+        webDriver = new HtmlUnitDriver(true);
+    }
 
     @Test
     public void testLoginFailure(){
@@ -22,5 +27,10 @@ public class LoginTest{
     public void testLoginSuccess(){
         HomePage homePage = PageFactory.initElements(webDriver, LoginPage.class).loginWithCorrectUserNamePassword();
         Assert.assertEquals(HomePage.WELCOME_MESSAGE, homePage.getWelcomeMessage());
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        webDriver.quit();
     }
 }
--- src/test/java/org/motechproject/tama/functional/tests/PatientRegistrationTest.java
+++ src/test/java/org/motechproject/tama/functional/tests/PatientRegistrationTest.java
@@ -1,20 +1,27 @@
 package org.motechproject.tama.functional.tests;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import junit.framework.Assert;
-import org.junit.Test;
+import org.junit.*;
 import org.motechproject.tama.Patient;
 import org.motechproject.tama.builders.PatientBuilder;
 import org.motechproject.tama.functional.pages.LoginPage;
 import org.motechproject.tama.functional.pages.ShowPatientPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.PageFactory;
 
 import java.text.SimpleDateFormat;
 
 public class PatientRegistrationTest {
-    private WebDriver webDriver = new FirefoxDriver();
+    private static WebDriver webDriver;
 
+    @BeforeClass
+    public static void setUp() {
+        webDriver = new HtmlUnitDriver(true);
+    }
 
     @Test
     public void testSuccessfulPatientRegistration() {
@@ -27,5 +34,11 @@ public class PatientRegistrationTest {
         Assert.assertEquals(showPatientPage.getMobileNumber(), patient.getMobilePhoneNumber());
         Assert.assertEquals(showPatientPage.getDateOfBirth(), new SimpleDateFormat("dd/MM/yyyy").format(patient.getDateOfBirth()));
     }
+
+    @AfterClass
+    public static void tearDown(){
+        webDriver.quit();
+    }
+
 
 }
