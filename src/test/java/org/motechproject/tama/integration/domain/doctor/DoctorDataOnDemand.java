09e89d5fd51f05abf6ca24fbeef0b889afe73d04
vijayaraj <vijayaraj@yahoo.com>
2011-07-06 17:22:03 +0530

    #2 Dumping Doctor Part2 <bajji/vijay

--- src/test/java/org/motechproject/tama/integration/domain/doctor/DoctorDataOnDemand.java
+++
@@ -1,8 +0,0 @@
-package org.motechproject.tama.integration.domain.doctor;
-
-import org.motechproject.tama.domain.Doctor;
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Doctor.class)
-public class DoctorDataOnDemand {
-}
