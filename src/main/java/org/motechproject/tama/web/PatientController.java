fd3d06bf4b33c00ddcc9fc74c46b561dc9952c3a
Sushant <sushants@thoughtworks.com>
2011-07-06 09:48:35 +0530

    Fixing PatientController compiler errors

--- src/main/java/org/motechproject/tama/web/PatientController.java
+++ src/main/java/org/motechproject/tama/web/PatientController.java
@@ -4,12 +4,6 @@ import org.motechproject.tama.TAMAConstants;
 import org.motechproject.tama.domain.Patient;
 import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
