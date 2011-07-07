1d3689bea8027a02554bb556b20526953e8ca370
prateek khatri <prateekk@thoughtworks.com>
2011-07-07 18:29:39 +0530

    #11 | wrote tests for findPatient method in PatientController. Removed the controller aspect. Removed the @ModelAttribute in the PAtientController. <Sanchit|Prateek>

--- src/main/java/org/motechproject/tama/web/PatientController.java
+++ src/main/java/org/motechproject/tama/web/PatientController.java
@@ -1,13 +1,27 @@
 package org.motechproject.tama.web;
 
+import org.joda.time.format.DateTimeFormat;
 import org.motechproject.tama.TAMAConstants;
+import org.motechproject.tama.domain.Gender;
+import org.motechproject.tama.domain.IVRLanguage;
 import org.motechproject.tama.domain.Patient;
+import org.motechproject.tama.repository.Patients;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriUtils;
+import org.springframework.web.util.WebUtils;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @RooWebScaffold(path = "patients", formBackingObject = Patient.class)
@@ -15,31 +29,168 @@ import java.util.List;
 @Controller
 public class PatientController {
 
+    public static final String PATIENT_ID_NOT_FOUND = "patientIdNotFound";
+    public static final String REDIRECT_SHOW_PATIENT = "redirect:/patients/";
+    public static final String REDIRECT_LIST_PATIENTS = "redirect:/patients";
+
+    @Autowired
+    Patients patients;
+
+    public PatientController() {
+    }
+
+    public PatientController(Patients patients) {
+        this.patients = patients;
+    }
+
     @RequestMapping(method = RequestMethod.POST, value = "/activate")
     public String activate(@RequestParam String id, HttpServletRequest httpServletRequest) {
         Patient.findPatient(id).activate().merge();
-        return "redirect:/patients/" + encodeUrlPathSegment(id, httpServletRequest);
+        return REDIRECT_SHOW_PATIENT + encodeUrlPathSegment(id, httpServletRequest);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/findByPatientId")
     public String findByPatientId(@RequestParam String patientId, Model uiModel, HttpServletRequest httpServletRequest) {
-        List<Patient> patients = Patient.findByPatientId(patientId);
-        if (patients == null || patients.isEmpty()) {
-            uiModel.addAttribute("patientIdNotFound", patientId);
+        List<Patient> patientList = patients.findByPatientId(patientId);
+        if (patientList == null || patientList.isEmpty()) {
+            uiModel.addAttribute(PATIENT_ID_NOT_FOUND, patientId);
             String referer = httpServletRequest.getHeader("Referer");
+            referer = referer.replaceFirst("&"+PATIENT_ID_NOT_FOUND+"=[0-9]*$", "");
+            referer = referer.replaceFirst("\\?"+PATIENT_ID_NOT_FOUND+"=[0-9]*$", "");
+
             return "redirect:"+referer;
         }
 
-        return "redirect:/patients/" + encodeUrlPathSegment(patients.get(0).getId(), httpServletRequest);
+        return REDIRECT_SHOW_PATIENT + encodeUrlPathSegment(patientList.get(0).getId(), httpServletRequest);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/activate/{id}")
     public String activate(@PathVariable String id) {
         Patient.findPatient(id).activate().merge();
-        return "redirect:/patients";
+        return REDIRECT_LIST_PATIENTS;
     }
 
-    @ModelAttribute("daysInAMonth")
+    @RequestMapping(method = RequestMethod.POST)
+    public String create(@Valid Patient patient, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+        if (bindingResult.hasErrors()) {
+            uiModel.addAttribute("patient", patient);
+            uiModel.addAttribute("genders", populateGenders());
+            uiModel.addAttribute("ivrlanguages", populateIVRLanguages());
+            uiModel.addAttribute("daysInAMonth", getDaysInAMonth());
+            uiModel.addAttribute("hoursInADay", getHoursInADay());
+            uiModel.addAttribute("minutesInAnHour", getMinutesInAnHour());
+            addDateTimeFormatPatterns(uiModel);
+                return "patients/create";
+        }
+        uiModel.asMap().clear();
+        patient.persist();
+        return REDIRECT_SHOW_PATIENT + encodeUrlPathSegment(patient.getId().toString(), httpServletRequest);
+    }
+
+    @RequestMapping(params = "form", method = RequestMethod.GET)
+    public String createForm(Model uiModel) {
+        uiModel.addAttribute("patient", new Patient());
+        uiModel.addAttribute("genders", populateGenders());
+        uiModel.addAttribute("ivrlanguages", populateIVRLanguages());
+        uiModel.addAttribute("daysInAMonth", getDaysInAMonth());
+        uiModel.addAttribute("hoursInADay", getHoursInADay());
+        uiModel.addAttribute("minutesInAnHour", getMinutesInAnHour());
+        addDateTimeFormatPatterns(uiModel);
+        return "patients/create";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String show(@PathVariable("id") String id, Model uiModel) {
+        addDateTimeFormatPatterns(uiModel);
+        uiModel.addAttribute("patient", Patient.findPatient(id));
+        uiModel.addAttribute("itemId", id);
+        return "patients/show";
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, Model uiModel) {
+        if (page != null || size != null) {
+            int sizeNo = size == null ? 10 : size.intValue();
+            uiModel.addAttribute("patients", Patient.findPatientEntries(page == null ? 0 : (page.intValue() - 1) * sizeNo, sizeNo));
+            float nrOfPages = (float) Patient.countPatients() / sizeNo;
+            uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+        } else {
+            uiModel.addAttribute("patients", populatePatients());
+        }
+        addDateTimeFormatPatterns(uiModel);
+        return "patients/list";
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public String update(@Valid Patient patient, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+        if (bindingResult.hasErrors()) {
+            uiModel.addAttribute("patient", patient);
+            uiModel.addAttribute("genders", populateGenders());
+            uiModel.addAttribute("ivrlanguages", populateIVRLanguages());
+            uiModel.addAttribute("daysInAMonth", getDaysInAMonth());
+            uiModel.addAttribute("hoursInADay", getHoursInADay());
+            uiModel.addAttribute("minutesInAnHour", getMinutesInAnHour());
+            addDateTimeFormatPatterns(uiModel);
+            return "patients/update";
+        }
+        uiModel.asMap().clear();
+        if(Patient.findPatient(patient.getId()).isActive())patient.activate();
+        patient.merge();
+        return REDIRECT_SHOW_PATIENT + encodeUrlPathSegment(patient.getId().toString(), httpServletRequest);
+    }
+
+
+    @RequestMapping(value = "/{id}", params = "form", method = RequestMethod.GET)
+    public String updateForm(@PathVariable("id") String id, Model uiModel) {
+        uiModel.addAttribute("patient", Patient.findPatient(id));
+        uiModel.addAttribute("genders", populateGenders());
+        uiModel.addAttribute("ivrlanguages", populateIVRLanguages());
+        uiModel.addAttribute("daysInAMonth", getDaysInAMonth());
+        uiModel.addAttribute("hoursInADay", getHoursInADay());
+        uiModel.addAttribute("minutesInAnHour", getMinutesInAnHour());
+        addDateTimeFormatPatterns(uiModel);
+        return "patients/update";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public String delete(@PathVariable("id") String id, @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, Model uiModel) {
+        Patient.findPatient(id).remove();
+        uiModel.asMap().clear();
+        uiModel.addAttribute("page", (page == null) ? "1" : page.toString());
+        uiModel.addAttribute("size", (size == null) ? "10" : size.toString());
+        uiModel.addAttribute("patients", populatePatients());
+        return REDIRECT_LIST_PATIENTS;
+    }
+
+
+    void addDateTimeFormatPatterns(Model uiModel) {
+        uiModel.addAttribute("patient_dateofbirth_date_format", DateTimeFormat.patternForStyle("S-", LocaleContextHolder.getLocale()));
+    }
+
+    String encodeUrlPathSegment(String pathSegment, HttpServletRequest httpServletRequest) {
+        String enc = httpServletRequest.getCharacterEncoding();
+        if (enc == null) {
+            enc = WebUtils.DEFAULT_CHARACTER_ENCODING;
+        }
+        try {
+            pathSegment = UriUtils.encodePathSegment(pathSegment, enc);
+        }
+        catch (UnsupportedEncodingException uee) {}
+        return pathSegment;
+    }
+
+    public Collection<Gender> populateGenders() {
+        return Gender.findAllGenders();
+    }
+
+    public Collection<IVRLanguage> populateIVRLanguages() {
+        return IVRLanguage.findAllIVRLanguages();
+    }
+
+    public Collection<Patient> populatePatients() {
+        return Patient.findAllPatients();
+    }
+
     public ArrayList<Integer> getDaysInAMonth() {
         ArrayList<Integer> daysInAMonth = new ArrayList<Integer>();
         for (int i = 0; i <= TAMAConstants.MAX_DAYS_IN_A_MONTH; i++) {
@@ -48,7 +199,6 @@ public class PatientController {
         return daysInAMonth;
     }
 
-    @ModelAttribute("hoursInADay")
     public ArrayList<Integer> getHoursInADay() {
         ArrayList<Integer> hoursInADay = new ArrayList<Integer>();
         for (int i = 0; i <= TAMAConstants.MAX_HOURS_IN_A_DAY; i++) {
@@ -57,7 +207,6 @@ public class PatientController {
         return hoursInADay;
     }
 
-    @ModelAttribute("minutesInAnHour")
     public ArrayList<Integer> getMinutesInAnHour() {
         ArrayList<Integer> minutesInAnHour = new ArrayList<Integer>();
         for (int i = 0; i <= TAMAConstants.MAX_MINUTES_IN_AN_HOUR; i++) {
--- src/test/java/org/motechproject/tama/web/PatientControllerTest.java
+++ src/test/java/org/motechproject/tama/web/PatientControllerTest.java
@@ -1,39 +1,48 @@
 package org.motechproject.tama.web;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.motechproject.tama.domain.Gender;
 import org.motechproject.tama.domain.Patient;
 import org.motechproject.tama.repository.Patients;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.constraints.AssertTrue;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(Patient.class)
+@PrepareForTest({Patient.class, Gender.class})
 public class PatientControllerTest {
 
     private PatientController controller;
+    private HttpServletRequest request;
+    private Model uiModel;
+    private Patients patients;
 
     @Before
     public void setUp() {
-        controller = new PatientController();
+        patients = mock(Patients.class);
+        controller = new PatientController(patients);
+        request = mock(HttpServletRequest.class);
+        uiModel = mock(Model.class);
     }
 
     @Test
     public void shouldActivatePatientsByPost() {
         PowerMockito.spy(Patient.class);
-        Patients patients = mock(Patients.class);
-        HttpServletRequest request = mock(HttpServletRequest.class);
 
         Patient dbPatient = mock(Patient.class);
         String id = "1234";
@@ -53,7 +62,6 @@ public class PatientControllerTest {
     @Test
     public void shouldActivatePatientsByGet() {
         PowerMockito.spy(Patient.class);
-        Patients patients = mock(Patients.class);
         Patient dbPatient = mock(Patient.class);
         String id = "1234";
         when(Patient.patients()).thenReturn(patients);
@@ -65,5 +73,74 @@ public class PatientControllerTest {
         verify(dbPatient).activate();
         verify(dbPatient).merge();
         assertEquals("redirect:/patients", nextPage);
+    }
+
+    @Test
+    public void shouldReturnToTheShowPatientPageIfPatientIsFound() {
+        String patientId = "123";
+        Patient patientFromDb = mock(Patient.class);
+        List<Patient> patientsFromDb = new ArrayList<Patient>();
+        patientsFromDb.add(patientFromDb);
+
+        when(patients.findByPatientId(patientId)).thenReturn(patientsFromDb);
+        when(patientFromDb.getId()).thenReturn("couchDbId");
+
+        String nextPage = controller.findByPatientId(patientId, uiModel, request);
+
+        assertEquals("redirect:/patients/couchDbId", nextPage);
+    }
+
+    @Test
+    public void shouldReturnToTheSamePageIfPatientIsNotFound() {
+        String patientId = "123";
+        String expectedPreviousPageUrl = "http://localhost:8080/tama/patients";
+        String previousPage = expectedPreviousPageUrl +"?patientIdNotFound=1234&patientIdNotFound=5678";
+
+        when(patients.findByPatientId(patientId)).thenReturn(new ArrayList<Patient>());
+        when(request.getHeader("Referer")).thenReturn(previousPage);
+
+        String nextPage = controller.findByPatientId(patientId, uiModel, request);
+
+        verify(uiModel).addAttribute(PatientController.PATIENT_ID_NOT_FOUND, patientId);
+        assertEquals("redirect:" + expectedPreviousPageUrl, nextPage);
+    }
+
+    @Test
+    public void shouldCreateAPatientIfThereAreNoErrors() {
+        Patient patientFromUI = mock(Patient.class);
+        BindingResult bindingResult = mock(BindingResult.class);
+        Map<String, Object> modelMap = new HashMap<String, Object>();
+        modelMap.put("dummyKey", "dummyValue");
+
+        when(bindingResult.hasErrors()).thenReturn(false);
+        when(patientFromUI.getId()).thenReturn("123");
+        when(uiModel.asMap()).thenReturn(modelMap);
+
+        String createPage = controller.create(patientFromUI, bindingResult, uiModel, request);
+
+        verify(patientFromUI).persist();
+        assertTrue(modelMap.isEmpty());
+        assertEquals("redirect:/patients/123", createPage);
+    }
+
+    @Test
+    @Ignore("needs to be fixed")
+    public void shouldCreateAPatientIfThereAreErrors() {
+//        PowerMockito.spy(Gender.class);
+        Patient patientFromUI = mock(Patient.class);
+        BindingResult bindingResult = mock(BindingResult.class);
+//        List<Gender> genders = new ArrayList<Gender>();
+//        genders.add(new Gender("Male"));
+
+        when(bindingResult.hasErrors()).thenReturn(true);
+//        when(Gender.findAllGenders()).thenReturn(genders);
+
+        String createPage = controller.create(patientFromUI, bindingResult, uiModel, request);
+
+        verify(uiModel).addAttribute("patient", patientFromUI);
+        verify(uiModel).addAttribute("patient_dateofbirth_date_format", "M/d/yy");
+//        verify(uiModel).addAttribute("genders", genders);
+        assertTrue(uiModel.containsAttribute("genders"));
+        assertEquals("patients/create", createPage);
     }
 }
