7cf5d49a9e3d326cfa28b1fa53283fd9a9d809a5
Alok Simha <lukasimha@gmail.com>
2011-07-07 10:53:27 +0530

    #20: Binding to a list of regimen compositions dynamically on change of regimen option <Shruthi|Luka>

--- src/main/java/org/motechproject/tama/web/TreatmentAdviceController.java
+++ src/main/java/org/motechproject/tama/web/TreatmentAdviceController.java
@@ -1,13 +1,16 @@
 package org.motechproject.tama.web;
 
-import java.util.List;
-
 import org.motechproject.tama.domain.Regimen;
+import org.motechproject.tama.domain.RegimenComposition;
 import org.motechproject.tama.domain.TreatmentAdvice;
 import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @RooWebScaffold(path = "treatmentadvices", formBackingObject = TreatmentAdvice.class)
 @RequestMapping("/treatmentadvices")
@@ -18,4 +21,26 @@ public class TreatmentAdviceController {
 	public List<Regimen> regimens() {
 		return Regimen.findAllRegimens();
 	}
+
+	@ModelAttribute("regimenCompositions")
+	public List<String> regimenCompositions(String regimenId) {
+        ArrayList<String> regimenCompositions = new ArrayList<String>();
+        for(Regimen regimen : regimens()) {
+            for (RegimenComposition composition : regimen.getCompositions()) {
+                regimenCompositions.add(composition.getRegimentCompositionId());
+            }
+        }
+        return regimenCompositions;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/regimenCompositionsFor")
+	public @ResponseBody Set<String> regimenCompositionsFor(@RequestParam String regimenId) {
+        Set<String> regimenCompositions = new HashSet<String>();
+        Set<RegimenComposition> compositions = Regimen.findRegimen(regimenId).getCompositions();
+        for (RegimenComposition composition : compositions) {
+            regimenCompositions.add(composition.getRegimentCompositionId());
+        }
+        return regimenCompositions;
+	}
+
 }
