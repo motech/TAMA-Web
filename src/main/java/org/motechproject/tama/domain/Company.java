a7d258a0e3e34892501df3b1ca9dfc00b8a445ca
Alok Simha <lukasimha@gmail.com>
2011-07-12 10:58:58 +0530

    #20: Removing auto-gen Company code <Shruthi|Luka>

--- src/main/java/org/motechproject/tama/domain/Company.java
+++ src/main/java/org/motechproject/tama/domain/Company.java
@@ -1,15 +1,15 @@
 package org.motechproject.tama.domain;
 
-import javax.validation.constraints.NotNull;
-
 import org.ektorp.support.TypeDiscriminator;
 import org.springframework.roo.addon.entity.RooEntity;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 
+import javax.validation.constraints.NotNull;
+
 @RooJavaBean
 @RooEntity
 @TypeDiscriminator("doc.documentType == 'Company'")
-public class Company {
+public class Company extends CouchEntity {
 
 	@NotNull
     private String name;
@@ -20,4 +20,12 @@ public class Company {
 	public Company(String name) {
 		this.name = name;
 	}
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/org/motechproject/tama/repository/Companies.java
+++ src/main/java/org/motechproject/tama/repository/Companies.java
@@ -1,31 +1,11 @@
 package org.motechproject.tama.repository;
 
 import org.ektorp.CouchDbConnector;
-import org.ektorp.ViewQuery;
-import org.ektorp.ViewResult;
-import org.ektorp.support.View;
 import org.motechproject.tama.domain.Company;
 
 public class Companies extends AbstractCouchRepository<Company> {
 
-
     public Companies(CouchDbConnector db) {
         super(Company.class, db);
     }
-
-    @View( name="count", map = "function(doc) { if (doc.documentType == 'Company') { emit(null, 1) } }", reduce="function(key, values, rereduce) { return sum(values); }")
-    public long count() {
-
-        ViewQuery query = createQuery("count");
-        ViewResult result = db.queryView(query);
-
-        int count = 0;
-        for (ViewResult.Row row : result.getRows()) {
-          count = row.getValueAsInt();
-          break;
-        }
-
-        return count;
-    }
-
 }
