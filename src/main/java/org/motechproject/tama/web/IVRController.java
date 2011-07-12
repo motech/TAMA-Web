b93c950a566f637a8bedf94205359c7eb222088f
prateek khatri <prateekk@thoughtworks.com>
2011-07-12 12:13:45 +0530

    #173 | Added check for max number of retries for session. added patient document id to session <Prateek>

--- src/main/java/org/motechproject/tama/web/IVRController.java
+++ src/main/java/org/motechproject/tama/web/IVRController.java
@@ -4,39 +4,72 @@ import org.motechproject.tama.TAMAConstants;
 import org.motechproject.tama.domain.Patient;
 import org.motechproject.tama.repository.Patients;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.support.RequestContext;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Properties;
 
 @RequestMapping("/ivr")
 @Controller
 public class IVRController {
     @Autowired
     private Patients patients;
-    public static final String NUM_OF_RETRIES = "num_of_retries";
 
-    public IVRController(Patients patients) {
+    @Qualifier("tamaProperties")
+    @Autowired
+    Properties properties;
+
+    public static final String NUM_OF_RETRIES = "num_of_retries";
+    public static final String PATIENT_DOCUMENT_ID = "patient_document_id";
+    public static final String MAX_NUM_OF_IVR_RETRIES_KEY = "MAX_NUM_OF_IVR_RETRIES";
+
+    public IVRController() {
+    }
+
+    public IVRController(Patients patients, Properties properties) {
         this.patients = patients;
+        this.properties = properties;
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/auth")
     public String authenticate(@RequestParam String phoneNumber, @RequestParam String passcode, HttpServletRequest request) {
-        HttpSession session = request.getSession(true);
+        HttpSession session = request.getSession();
 
         Patient patient = patients.findByMobileNumber(phoneNumber);
         if (passcode.equals(patient.getPasscode())) {
-            //TODO: Replace the following with the correecct URL while integrating
+            session.invalidate();
+            session = request.getSession();
+            session.setAttribute(PATIENT_DOCUMENT_ID, patient.getId());
+
+            //TODO: Replace the following with the correct URL while integrating
             return TAMAConstants.AUTH_STATUS.AUTHENTICATED.getValue();
         }
 
-        Object numOfRetriesAttrValue = session.getAttribute(NUM_OF_RETRIES);
-        int numOfRetries = numOfRetriesAttrValue == null ? 0 : (Integer)numOfRetriesAttrValue;
-        session.setAttribute(NUM_OF_RETRIES, numOfRetries+1);
+        int numOfRetries = getNumberOfRetries(session);
+        if(thisIsTheLastTry(numOfRetries)) {
+            session.invalidate();
+        } else {
+            session.setAttribute(NUM_OF_RETRIES, numOfRetries + 1);
+        }
+
+        //TODO: Replace the following with the correct URL while integrating
         return TAMAConstants.AUTH_STATUS.UNAUTHENTICATED.getValue();
+    }
+
+    private int getNumberOfRetries(HttpSession session) {
+        Object numOfRetriesAttrValue = session.getAttribute(NUM_OF_RETRIES);
+        return numOfRetriesAttrValue == null ? 0 : (Integer) numOfRetriesAttrValue;
+    }
+
+    private boolean thisIsTheLastTry(int numOfRetries) {
+        return properties.getProperty(MAX_NUM_OF_IVR_RETRIES_KEY).equals(String.valueOf(numOfRetries + 1));
     }
 }
--- src/test/java/org/motechproject/tama/web/IVRControllerTest.java
+++ src/test/java/org/motechproject/tama/web/IVRControllerTest.java
@@ -2,19 +2,22 @@ package org.motechproject.tama.web;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.Matchers;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.motechproject.tama.TAMAConstants;
 import org.motechproject.tama.builder.PatientBuilder;
 import org.motechproject.tama.domain.Patient;
 import org.motechproject.tama.repository.Patients;
+import org.springframework.context.MessageSource;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import java.util.Properties;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static org.powermock.api.mockito.PowerMockito.verifyNew;
 import static org.powermock.api.mockito.PowerMockito.when;
@@ -31,13 +34,15 @@ public class IVRControllerTest {
     private HttpServletRequest request;
     @Mock
     private HttpSession session;
+    @Mock
+    private Properties properties;
 
     private Patient patientFromDb;
 
     @Before
     public void setup() {
         initMocks(this);
-        controller = new IVRController(patients);
+        controller = new IVRController(patients, properties);
 
         patientFromDb = new PatientBuilder()
                 .withDefaults()
@@ -47,19 +52,30 @@ public class IVRControllerTest {
     }
 
     @Test
-    public void shouldCreateANewSessionForTheFirstRequest() {
+    public void shouldCreateANewSessionForTheFirstRequest_Authentication() {
 
         when(patients.findByMobileNumber(MOBILE_NUMBER)).thenReturn(patientFromDb);
+        when(request.getSession()).thenReturn(session);
 
         controller.authenticate(MOBILE_NUMBER, PASSCODE,  request);
+        verify(request, times(2)).getSession();
+    }
 
-        verify(request).getSession(true);
+    @Test
+    public void ShouldCreateANewSessionForTheFirstRequest_Unauthentication() {
+        when(patients.findByMobileNumber(MOBILE_NUMBER)).thenReturn(patientFromDb);
+        when(request.getSession()).thenReturn(session);
+        when(properties.getProperty(IVRController.MAX_NUM_OF_IVR_RETRIES_KEY)).thenReturn(String.valueOf(1));
+
+        controller.authenticate(MOBILE_NUMBER, "wrong" + PASSCODE, request);
+        verify(request, times(1)).getSession();
     }
 
     @Test
     public void shouldAuthenticateAPatientWithCorrectPasscode() {
 
         when(patients.findByMobileNumber(MOBILE_NUMBER)).thenReturn(patientFromDb);
+        when(request.getSession()).thenReturn(session);
 
         String authStatus = controller.authenticate(MOBILE_NUMBER, PASSCODE, request);
 
@@ -70,7 +86,8 @@ public class IVRControllerTest {
     public void shouldNotAuthenticateAPatientWithIncorrectPasscode() {
 
         when(patients.findByMobileNumber(MOBILE_NUMBER)).thenReturn(patientFromDb);
-        when(request.getSession(true)).thenReturn(session);
+        when(request.getSession()).thenReturn(session);
+        when(properties.getProperty(IVRController.MAX_NUM_OF_IVR_RETRIES_KEY)).thenReturn(String.valueOf(1));
 
         String authStatus = controller.authenticate(MOBILE_NUMBER, "wrong"+PASSCODE, request);
 
@@ -81,7 +98,8 @@ public class IVRControllerTest {
     public void shouldStoreNumberOfAuthenticationRetriesInTheSession() {
 
         when(patients.findByMobileNumber(MOBILE_NUMBER)).thenReturn(patientFromDb);
-        when(request.getSession(true)).thenReturn(session);
+        when(request.getSession()).thenReturn(session);
+        when(properties.getProperty(IVRController.MAX_NUM_OF_IVR_RETRIES_KEY)).thenReturn(String.valueOf(3));
 
         controller.authenticate(MOBILE_NUMBER, "wrong"+PASSCODE, request);
         verify(session).setAttribute(IVRController.NUM_OF_RETRIES, 1);
@@ -90,9 +108,65 @@ public class IVRControllerTest {
         controller.authenticate(MOBILE_NUMBER, "wrong" + PASSCODE, request);
         verify(session).setAttribute(IVRController.NUM_OF_RETRIES, 2);
     }
+
+    @Test
+    public void shouldStorePatientDocumentIdInTheSessionWhenAuthenticated() {
+
+        String patientDocumentId = "1234";
+        patientFromDb.setId(patientDocumentId);
+
+        when(patients.findByMobileNumber(MOBILE_NUMBER)).thenReturn(patientFromDb);
+        when(request.getSession()).thenReturn(session);
+        when(properties.getProperty(IVRController.MAX_NUM_OF_IVR_RETRIES_KEY)).thenReturn(String.valueOf(2));
+
+        controller.authenticate(MOBILE_NUMBER, PASSCODE, request);
+        verify(session).setAttribute(IVRController.PATIENT_DOCUMENT_ID, patientDocumentId);
+    }
+
+    @Test
+    public void shouldNotStorePatientDocumentIdInTheSessionWhenUnauthenticated() {
+
+        String patientDocumentId = "1234";
+        patientFromDb.setId(patientDocumentId);
+
+        when(patients.findByMobileNumber(MOBILE_NUMBER)).thenReturn(patientFromDb);
+        when(request.getSession()).thenReturn(session);
+        when(properties.getProperty(IVRController.MAX_NUM_OF_IVR_RETRIES_KEY)).thenReturn(String.valueOf(2));
+
+        controller.authenticate(MOBILE_NUMBER, "wrong"+PASSCODE, request);
+        verify(session, never()).setAttribute(IVRController.PATIENT_DOCUMENT_ID, patientDocumentId);
+    }
+
+    @Test
+    public void shouldDiscardTheOldSessionAndCreateANewOneAfterSuccessfulAuthentication() {
+
+        when(patients.findByMobileNumber(MOBILE_NUMBER)).thenReturn(patientFromDb);
+        when(request.getSession()).thenReturn(session);
+
+        controller.authenticate(MOBILE_NUMBER, PASSCODE,  request);
+
+        InOrder inOrder = inOrder(session, request);
+        inOrder.verify(session).invalidate();
+        inOrder.verify(request).getSession();
+    }
     
     @Test
-    public void shouldAllowMaximumRetriesAsPerTheConfiguredValue() {
-        
+    public void shouldInvalidateSessionAfterMaximumRetries() {
+        when(patients.findByMobileNumber(MOBILE_NUMBER)).thenReturn(patientFromDb);
+        when(request.getSession()).thenReturn(session);
+        when(session.getAttribute(IVRController.NUM_OF_RETRIES))
+                .thenReturn(null)
+                .thenReturn(1)
+                .thenReturn(2);
+        when(properties.getProperty(IVRController.MAX_NUM_OF_IVR_RETRIES_KEY)).thenReturn(String.valueOf(3));
+
+        controller.authenticate(MOBILE_NUMBER, "wrong"+PASSCODE, request);
+        verify(session, never()).invalidate();
+
+        controller.authenticate(MOBILE_NUMBER, "wrong"+PASSCODE, request);
+        verify(session, never()).invalidate();
+
+        controller.authenticate(MOBILE_NUMBER, "wrong"+PASSCODE, request);
+        verify(session).invalidate();
     }
 }
