9db2028bd89d7f4a49120563396802a5f3808fd4
Alok Simha <lukasimha@gmail.com>
2011-07-13 08:11:52 +0530

    #20: Added view on TreatmentAdvice to fetch given a patientId <Shruthi|Luka>

--- src/main/java/org/motechproject/tama/repository/TreatmentAdvices.java
+++ src/main/java/org/motechproject/tama/repository/TreatmentAdvices.java
@@ -1,9 +1,12 @@
 package org.motechproject.tama.repository;
 
 import org.ektorp.CouchDbConnector;
+import org.ektorp.ViewQuery;
 import org.ektorp.support.CouchDbRepositorySupport;
 import org.ektorp.support.View;
 import org.motechproject.tama.domain.TreatmentAdvice;
+
+import java.util.List;
 
 @View( name="all", map = "function(doc) { if (doc.documentType == 'TreatmentAdvice') { emit(null, doc) } }")
 public class TreatmentAdvices extends CouchDbRepositorySupport<TreatmentAdvice> {
@@ -11,5 +14,12 @@ public class TreatmentAdvices extends CouchDbRepositorySupport<TreatmentAdvice> 
     public TreatmentAdvices(CouchDbConnector db) {
         super(TreatmentAdvice.class, db);
         initStandardDesignDocument();
+    }
+
+    @View(name = "find_by_patient_id", map = "function(doc) {if (doc.documentType =='TreatmentAdvice' && doc.patientId) {emit(doc.patientId, doc._id);}}")
+    public TreatmentAdvice findByPatientId(String patientId) {
+        ViewQuery q = createQuery("find_by_patient_id").key(patientId).includeDocs(true);
+        List<TreatmentAdvice> treatmentAdvices = db.queryView(q, TreatmentAdvice.class);
+        return treatmentAdvices.isEmpty() ? null : treatmentAdvices.get(0);
     }
 }
---
+++ src/test/java/org/motechproject/tama/integration/TreatmentAdvicesTest.java
@@ -0,0 +1,35 @@
+package org.motechproject.tama.integration;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.motechproject.tama.domain.TreatmentAdvice;
+import org.motechproject.tama.integration.domain.SpringIntegrationTest;
+import org.motechproject.tama.repository.TreatmentAdvices;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TreatmentAdvicesTest extends SpringIntegrationTest {
+
+    @Autowired
+    TreatmentAdvices treatmentAdvices;
+
+    @Test
+    public void testFindByPatientId() {
+        TreatmentAdvice treatmentAdvice = new TreatmentAdvice();
+        treatmentAdvice.setPatientId("111111");
+        treatmentAdvices.add(treatmentAdvice);
+
+        TreatmentAdvice retrievedTreatmentAdvice = treatmentAdvices.findByPatientId("111111");
+
+        Assert.assertNotNull(retrievedTreatmentAdvice);
+        Assert.assertEquals("111111", retrievedTreatmentAdvice.getPatientId());
+
+        markForDeletion(treatmentAdvice);
+    }
+
+    @Test
+    public void testFindByPatientIdReturnsNoTreatmentAdvice() {
+        String invalidPatientId = "999999";
+        TreatmentAdvice retrievedTreatmentAdvice = treatmentAdvices.findByPatientId(invalidPatientId);
+        Assert.assertNull(retrievedTreatmentAdvice);
+    }
+}
