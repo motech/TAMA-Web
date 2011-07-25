67a38ee4000d53f4b559450a3224cb7819eb5756
Sushant <sushant.srivastav@gmail.com>
2011-07-25 19:48:18 +0530

    [BalajiN, Sushant] #35, making pill reminder module listen to the event queue

--- src/main/java/org/motechproject/tama/listener/Initiator.java
+++ src/main/java/org/motechproject/tama/listener/Initiator.java
@@ -1,6 +1,7 @@
 package org.motechproject.tama.listener;
 
 import org.motechproject.server.event.annotations.EventAnnotationBeanPostProcessor;
+import org.motechproject.server.pillreminder.service.PillReminderServiceImpl;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -24,6 +25,7 @@ public class Initiator implements ServletContextListener{
         WebApplicationContext webApplicationContext = WebApplicationContextUtils.getWebApplicationContext(servletContext);
         HashMap<String, Object> beans = new HashMap<String, Object>();
         beans.put(PillReminderListener.class.getName(), webApplicationContext.getBean(PillReminderListener.class));
+        beans.put(PillReminderServiceImpl.class.getName(), webApplicationContext.getBean(PillReminderServiceImpl.class));
         return beans;
     }
 
