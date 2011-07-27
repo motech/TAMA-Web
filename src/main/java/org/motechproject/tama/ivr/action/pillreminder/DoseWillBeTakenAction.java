eadecb0a84e431536eaae2016e7621bddecb910c
vijayaraj <vijayaraj@yahoo.com>
2011-07-27 16:18:19 +0530

    #31 Fixed DoseWillBeTakenAction <mano/vijay>

--- src/main/java/org/motechproject/tama/ivr/action/pillreminder/DoseWillBeTakenAction.java
+++ src/main/java/org/motechproject/tama/ivr/action/pillreminder/DoseWillBeTakenAction.java
@@ -6,6 +6,7 @@ import org.motechproject.tama.ivr.IVRRequest;
 import org.motechproject.tama.ivr.action.BaseIncomingAction;
 import org.motechproject.tama.ivr.builder.IVRResponseBuilder;
 import org.motechproject.tama.repository.IVRCallAudits;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 public class DoseWillBeTakenAction extends BaseIncomingAction {
     public static final String KEY = "2";
 
+    @Autowired
     public DoseWillBeTakenAction(IVRMessage messages, IVRCallAudits audits) {
         this.messages = messages;
         this.audits = audits;
