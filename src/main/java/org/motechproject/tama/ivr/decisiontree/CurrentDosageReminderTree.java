0fef500067db1a12cd295273dc627b81fd38644b
Alok Simha <lukasimha@gmail.com>
2011-08-08 19:24:24 +0530

    #0000: Added qualifier for UpdateAdherenceCommand <Luka>

--- src/main/java/org/motechproject/tama/ivr/decisiontree/CurrentDosageReminderTree.java
+++ src/main/java/org/motechproject/tama/ivr/decisiontree/CurrentDosageReminderTree.java
@@ -26,6 +26,7 @@ public class CurrentDosageReminderTree extends TAMADecisionTree {
     @Qualifier("stopTodaysRemindersCommand")
     @Autowired
     private StopTodaysRemindersCommand stopTodaysRemindersCommand;
+    @Qualifier("updateAdherenceCommand")
     @Autowired
     private UpdateAdherenceCommand updateAdherenceCommand;
     @Autowired
