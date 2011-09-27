46b715dc0e3f10999a979144b6568554a5b0d87d
Shruthi <shruthi@thoughtworks.com>
2011-09-27 17:31:58 +0530

    Raja,Shruthi|#567-Outbox story|Made setup.outbox messages more generic. Can now send patient id and wav file names to be setup.

--- src/main/java/org/motechproject/tama/tools/SetupOutboxMessages.java
+++ src/main/java/org/motechproject/tama/tools/SetupOutboxMessages.java
@@ -16,25 +16,34 @@ public class SetupOutboxMessages {
     public static final String APPLICATION_CONTEXT_XML = "META-INF/spring/applicationContext-tools.xml";
 
     public static void main(String[] args) {
+
+        System.out.println("Args length: " + args.length);
+
+        if (args.length < 2) {
+            System.err.println("Invalid arguments. Specify patient ID followed by wav file name without extension. Ex: 111 Num_000 Num_001");
+            return;
+        }
+
         ApplicationContext context = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_XML);
         OutboundVoiceMessageDao outboxRepository = context.getBean(OutboundVoiceMessageDao.class);
         AllPatients patientRepository = context.getBean(AllPatients.class);
 
-        Patient patient = patientRepository.findByPatientId("1111").get(0);
-        int messageCount = Integer.parseInt("4");
+        System.out.println("Finding patient for ID:" + args[0] + ".");
+        Patient patient = patientRepository.findByPatientId(args[0]).get(0);
 
-        for (int count = 0; count < messageCount; count++) {
+        for (int i = 1; i < args.length; i++) {
+            System.out.println(String.format("Adding message %s", args[i]));
+
             OutboundVoiceMessage outboundVoiceMessage = new OutboundVoiceMessage();
             outboundVoiceMessage.setPartyId(patient.getId());
             outboundVoiceMessage.setCreationTime(DateUtil.now().toDate());
             outboundVoiceMessage.setExpirationDate(DateUtil.tomorrow().toDate());
             outboundVoiceMessage.setStatus(OutboundVoiceMessageStatus.PENDING);
             HashMap<String, Object> map = new HashMap<String, Object>();
-            map.put("audioFiles", Arrays.asList(String.format("Num_00%d", count)));
+            map.put("audioFiles", Arrays.asList(args[i]));
             outboundVoiceMessage.setParameters(map);
 
             outboxRepository.add(outboundVoiceMessage);
-            System.out.println(String.format("Added message %d", count));
         }
     }
 }
