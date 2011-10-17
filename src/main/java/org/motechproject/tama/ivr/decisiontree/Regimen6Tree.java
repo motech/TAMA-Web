595afc982c14e2d03247da7cb68664bdac552c1b
Dileep Bapat <dileepbapat@gmail.com>
2011-10-17 12:24:55 +0530

    BalajiN,Dileep | Adding new symptom tree to prod. now tree includes 1 to 6 regimen.

--- src/main/java/org/motechproject/tama/ivr/decisiontree/Regimen6Tree.java
+++ src/main/java/org/motechproject/tama/ivr/decisiontree/Regimen6Tree.java
@@ -9,7 +9,7 @@ public class Regimen6Tree extends TamaDecisionTree {
 	@Override
 	protected Node createRootNode() {
 		try {
-			Class builderClass = Class.forName("org.motechproject.tama.ivr.decisiontree.Regimen6TreeBuilder");
+			Class builderClass = Class.forName("org.motechproject.tama.ivr.decisiontree.Regimen1To6TreeBuilder");
 			return (Node)builderClass.getMethod("getRootNode", new Class[0]).invoke(null);
 		} catch (Exception e) {
 			return null;
