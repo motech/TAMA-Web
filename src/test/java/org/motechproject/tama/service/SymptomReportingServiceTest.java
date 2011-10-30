93a03381be9b63a5699c46ad7010fd3b04b30081
Shruthi <shruthi@thoughtworks.com>
2011-10-30 16:22:57 +0530

    Shruthi|#234|Test for all Regimen 4 symptoms reporting scenarios.

--- src/test/java/org/motechproject/tama/service/SymptomReportingServiceTest.java
+++ src/test/java/org/motechproject/tama/service/SymptomReportingServiceTest.java
@@ -70,8 +70,44 @@ public class SymptomReportingServiceTest extends SpringIntegrationTest {
     }
 
     @Test
+    public void shouldReturnRegimen4_1Tree() {
+        medicalCondition.regimenName("Regimen IV").cd4Count(10).psychiatricIllness(false).lowBaselineHBCount(true);
+        assertEquals("Regimen4_1_6", execute());
+    }
+
+    @Test
+    public void shouldReturnRegimen4_2Tree() {
+        medicalCondition.regimenName("Regimen IV").cd4Count(90).psychiatricIllness(false).lowBaselineHBCount(true);
+        assertEquals("Regimen4_2", execute());
+    }
+
+    @Test
+    public void shouldReturnRegimen4_3Tree() {
+        medicalCondition.regimenName("Regimen IV").cd4Count(10).psychiatricIllness(true).lowBaselineHBCount(false);
+        assertEquals("Regimen4_3", execute());
+    }
+
+    @Test
+    public void shouldReturnRegimen4_4Tree() {
+        medicalCondition.regimenName("Regimen IV").cd4Count(90).psychiatricIllness(true).lowBaselineHBCount(false);
+        assertEquals("Regimen4_4", execute());
+    }
+
+    @Test
     public void shouldReturnRegimen4_5Tree() {
         medicalCondition.regimenName("Regimen IV").cd4Count(10).psychiatricIllness(false).lowBaselineHBCount(false);
         assertEquals("Regimen4_5", execute());
+    }
+
+    @Test
+    public void shouldReturnRegimen4_6Tree() {
+        medicalCondition.regimenName("Regimen IV").cd4Count(10).psychiatricIllness(true).lowBaselineHBCount(true);
+        assertEquals("Regimen4_1_6", execute());
+    }
+
+    @Test
+    public void shouldReturnRegimen4_7Tree() {
+        medicalCondition.regimenName("Regimen IV").cd4Count(90).psychiatricIllness(true).lowBaselineHBCount(true);
+        assertEquals("Regimen4_7", execute());
     }
 }
