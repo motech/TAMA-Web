85e50d3f40d58f609c77ebf98f3fa174ccfc6840
Alok Simha <lukasimha@gmail.com>
2011-10-31 16:00:00 +0530

    <Sri|Luka> #668: Added Regimen5 drl to applicationContext

---
+++ src/main/java/org/motechproject/tama/domain/ruleinferences/Regimen5.java
@@ -0,0 +1,9 @@
+package org.motechproject.tama.domain.ruleinferences;
+
+import org.motechproject.tama.domain.MedicalCondition;
+
+public class Regimen5 extends PatientMedicalConditionInference {
+    public Regimen5(MedicalCondition medicalCondition) {
+        super(medicalCondition);
+    }
+}
