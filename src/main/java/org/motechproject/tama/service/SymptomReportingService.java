88389cca45be5ad411c590ca7876b37dc099c45a
Alok Simha <lukasimha@gmail.com>
2011-11-02 14:50:50 +0530

    <Luka> #688: Drool's condition should not match more than one tree

--- src/main/java/org/motechproject/tama/service/SymptomReportingService.java
+++ src/main/java/org/motechproject/tama/service/SymptomReportingService.java
@@ -1,6 +1,7 @@
 package org.motechproject.tama.service;
 
 import org.drools.runtime.StatelessKnowledgeSession;
+import org.motechproject.tama.TamaException;
 import org.motechproject.tama.domain.MedicalCondition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,13 +10,23 @@ import java.util.ArrayList;
 
 @Component
 public class SymptomReportingService {
-    @Autowired
     private StatelessKnowledgeSession ksession;
+    private ArrayList<String> tree;
+
+    @Autowired
+    public SymptomReportingService(StatelessKnowledgeSession ksession) {
+        this(ksession, new ArrayList<String>());
+    }
+
+    public SymptomReportingService(StatelessKnowledgeSession ksession, ArrayList<String> tree) {
+        this.ksession = ksession;
+        this.tree = tree;
+    }
 
     public String getSymptomReportingTree(MedicalCondition medicalCondition) {
-        ArrayList<String> tree = new ArrayList<String>();
         ksession.setGlobal("tree", tree);
         ksession.execute(medicalCondition);
+        if (tree.size() > 1) throw new TamaException("Should not match more than one tree condition.");
         return tree.size() == 0 ? null : tree.get(0);
     }
 }
