537ce509ee27f992750b51ab8ca91a79b9701ba2
Shruthi <shruthi@thoughtworks.com>
2011-11-02 12:40:43 +0530

    BalajiN,Shruthi|#733|Changed trigger.four.day.recall.event target to trigger a call instead of calling handle on FourDayRecallHandler

--- src/main/java/org/motechproject/tama/tools/TriggerFourDayRecallEvent.java
+++ src/main/java/org/motechproject/tama/tools/TriggerFourDayRecallEvent.java
@@ -1,18 +1,10 @@
 package org.motechproject.tama.tools;
 
-import org.motechproject.model.MotechEvent;
-import org.motechproject.tama.TAMAConstants;
 import org.motechproject.tama.domain.Patient;
-import org.motechproject.tama.domain.TreatmentAdvice;
-import org.motechproject.tama.listener.FourDayRecallListener;
-import org.motechproject.tama.platform.service.FourDayRecallEventPayloadBuilder;
+import org.motechproject.tama.ivr.call.IvrCall;
 import org.motechproject.tama.repository.AllPatients;
-import org.motechproject.tama.repository.AllTreatmentAdvices;
-import org.motechproject.util.DateUtil;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.Map;
 
 public class TriggerFourDayRecallEvent {
     public static final String APPLICATION_CONTEXT_XML = "META-INF/spring/applicationContext.xml";
@@ -22,21 +14,10 @@ public class TriggerFourDayRecallEvent {
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_XML);
         AllPatients allPatients = applicationContext.getBean(AllPatients.class);
-        AllTreatmentAdvices allTreatmentAdvices = applicationContext.getBean(AllTreatmentAdvices.class);
-
         Patient patient = allPatients.findByPatientId(patientId).get(0);
-        TreatmentAdvice treatmentAdvice = allTreatmentAdvices.findByPatientId(patient.getId());
-
-        Map<String, Object> eventParams = new FourDayRecallEventPayloadBuilder()
-                .withPatientDocId(patient.getId())
-                .withTreatmentAdviceId(treatmentAdvice.getId())
-                .withTreatmentAdviceStartDate(DateUtil.newDate(treatmentAdvice.getStartDate()))
-                .withRetryFlag(false)
-                .payload();
-        MotechEvent fourDayRecallEvent = new MotechEvent(TAMAConstants.FOUR_DAY_RECALL_SUBJECT, eventParams);
 
         System.out.println("Triggering the FourDayRecall event...");
-        FourDayRecallListener fourDayRecallListener = applicationContext.getBean(FourDayRecallListener.class);
-        fourDayRecallListener.handle(fourDayRecallEvent);
+        IvrCall ivrCall = applicationContext.getBean(IvrCall.class);
+        ivrCall.makeCall(patient.getId());
     }
 }
