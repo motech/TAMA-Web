cde574d03e60c5d8ebc6816ed3789c13a48b76cc
Dileep Bapat <dileepbapat@gmail.com>
2011-11-08 16:23:01 +0530

    <Balaji/Dileep/Luka> #0000: tama-modules pom changes and fixing integration test.

--- presentation/src/test/java/org/motechproject/tama/integration/decisiontree/CurrentDosageReminderTreeTest.java
+++ presentation/src/test/java/org/motechproject/tama/integration/decisiontree/CurrentDosageReminderTreeTest.java
@@ -29,7 +29,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"/applicationContext.xml"})
+@ContextConfiguration(locations = "classpath*:**/applicationContext.xml")
 public class CurrentDosageReminderTreeTest {
     @Autowired
     private TestTree currentDosageReminderTree;
