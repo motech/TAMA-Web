e4cd1d1773175203c18fb4193803acf1ed6c880d
shruthi <shruthidipali@gmail.com>
2011-11-08 18:31:42 +0530

    Manohar,Shruthi|#710|Playing menu as a MenuAudioPrompt in FourDayRecallIncomingCallTree

--- presentation/src/main/java/org/motechproject/tama/ivr/decisiontree/FourDayRecallIncomingCallTree.java
+++ presentation/src/main/java/org/motechproject/tama/ivr/decisiontree/FourDayRecallIncomingCallTree.java
@@ -17,7 +17,7 @@ public class FourDayRecallIncomingCallTree extends TamaDecisionTree {
         return new Node()
                 .setPrompts(
                         new AudioPrompt().setCommand(welcomeGreetingMessage),
-                        new AudioPrompt().setName(TamaIVRMessage.MENU_010_05_01_MAINMENU4)
+                        new MenuAudioPrompt().setName(TamaIVRMessage.MENU_010_05_01_MAINMENU4)
                 )
                 .setTransitions(new Object[][]{
                         {"2", TAMATransitionFactory.createCallStateTransition(CallState.SYMPTOM_REPORTING)},
