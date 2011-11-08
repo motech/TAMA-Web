42e9b23e0117dac2e2fa5022281df37f99c2f660
shruthi <shruthidipali@gmail.com>
2011-11-08 17:09:12 +0530

    Manohar,Shruthi|#668|adherence percentage is also posted with adherence trend outbox message.

--- presentation/src/main/java/org/motechproject/tama/ivr/VoiceMessageResponseFactory.java
+++ presentation/src/main/java/org/motechproject/tama/ivr/VoiceMessageResponseFactory.java
@@ -1,33 +1,31 @@
 package org.motechproject.tama.ivr;
 
+import org.motechproject.ivr.kookoo.KooKooIVRContext;
 import org.motechproject.ivr.kookoo.KookooIVRResponseBuilder;
 import org.motechproject.outbox.api.model.OutboundVoiceMessage;
 import org.motechproject.outbox.api.model.VoiceMessageType;
 import org.motechproject.tama.outbox.OutboxContext;
-import org.motechproject.tama.web.OutboxController;
+import org.motechproject.tama.web.command.MessageForAdherenceWhenPreviousDosageNotCapturedCommand;
 import org.motechproject.tama.web.command.PlayAdherenceTrendFeedbackCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 public class VoiceMessageResponseFactory {
-    private PlayAdherenceTrendFeedbackCommand command;
+    private PlayAdherenceTrendFeedbackCommand adherenceTrendCommand;
+    private MessageForAdherenceWhenPreviousDosageNotCapturedCommand adherencePercentageCommand;
 
     @Autowired
-    public VoiceMessageResponseFactory(PlayAdherenceTrendFeedbackCommand command) {
-        this.command = command;
+    public VoiceMessageResponseFactory(PlayAdherenceTrendFeedbackCommand command, MessageForAdherenceWhenPreviousDosageNotCapturedCommand adherencePercentageCommand) {
+        this.adherenceTrendCommand = command;
+        this.adherencePercentageCommand = adherencePercentageCommand;
     }
 
-    public void voiceMessageResponse(OutboxContext outboxContext, OutboundVoiceMessage outboundVoiceMessage, KookooIVRResponseBuilder ivrResponseBuilder) {
+    public void voiceMessageResponse(KooKooIVRContext kooKooIVRContext, OutboxContext outboxContext, OutboundVoiceMessage outboundVoiceMessage, KookooIVRResponseBuilder ivrResponseBuilder) {
         VoiceMessageType voiceMessageType = outboundVoiceMessage.getVoiceMessageType();
         if (voiceMessageType != null && "AudioCommand".equals(voiceMessageType.getVoiceMessageTypeName())) {
-            List<String> commands = (List<String>) outboundVoiceMessage.getParameters().get(OutboxController.VOICE_MESSAGE_COMMAND);
-            for (String s : commands) {
-                ivrResponseBuilder.withPlayAudios(command.execute(outboxContext));
-            }
+            ivrResponseBuilder.withPlayAudios(adherencePercentageCommand.execute(kooKooIVRContext));
+            ivrResponseBuilder.withPlayAudios(adherenceTrendCommand.execute(outboxContext));
         }
     }
-
 }
