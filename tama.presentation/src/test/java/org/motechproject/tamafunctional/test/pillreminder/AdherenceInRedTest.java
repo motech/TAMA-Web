827062bf72cf3630a9ddd9da332f58eba843cfdb
Alok Simha <lukasimha@gmail.com>
2011-12-13 20:40:31 +0530

    <Luka> #0000: Added the AdherenceInRed functional test. 1. Asserts only for 0 adherence. 2. Still need to fake-time and run the adherence job, to ensure red-alert is created

---
+++ tama.presentation/src/test/java/org/motechproject/tamafunctional/test/pillreminder/AdherenceInRedTest.java
@@ -0,0 +1,55 @@
+package org.motechproject.tamafunctional.test.pillreminder;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.motechproject.tamafunctional.test.ivr.BaseIVRTest;
+import org.motechproject.tamafunctional.testdata.PillReminderCallInfo;
+import org.motechproject.tamafunctional.testdata.TestClinician;
+import org.motechproject.tamafunctional.testdata.TestPatient;
+import org.motechproject.tamafunctional.testdata.ivrreponse.IVRResponse;
+import org.motechproject.tamafunctional.testdata.treatmentadvice.TestDrugDosage;
+import org.motechproject.tamafunctional.testdata.treatmentadvice.TestTreatmentAdvice;
+import org.motechproject.tamafunctional.testdataservice.PatientDataService;
+import org.motechproject.tamafunctional.testdataservice.ScheduledJobDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
+
+import static org.motechproject.tamacallflow.ivr.TamaIVRMessage.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath*:**/applicationFunctionalTestContext.xml")
+public class AdherenceInRedTest extends BaseIVRTest {
+    @Autowired
+    private ScheduledJobDataService scheduledJobDataService;
+    private TestPatient patient;
+
+    @Before
+    public void setUp() {
+        super.setUp();
+        TestClinician clinician = TestClinician.withMandatory();
+        patient = TestPatient.withMandatory();
+        TestTreatmentAdvice treatmentAdvice = TestTreatmentAdvice.withExtrinsic(TestDrugDosage.create("Efferven", "Combivir"));
+
+        PatientDataService patientDataService = new PatientDataService(webDriver);
+        patientDataService.setupARTRegimenWithDependents(treatmentAdvice, patient, clinician);
+        caller = caller(patient);
+    }
+
+    @Test
+    public void shouldRaise_RedAlert_WhenAdherenceFalls_Below70Percent() throws IOException {
+        String currentDosageId = scheduledJobDataService.currentDosageId(patient.id());
+        logInfo("{CurrentDosageId}{Id={%s}}", currentDosageId);
+
+        caller.replyToCall(new PillReminderCallInfo(currentDosageId, 1));
+        IVRResponse ivrResponse = caller.enter("1234");
+        asksForCollectDtmfWith(ivrResponse, PILL_REMINDER_RESPONSE_MENU, ITS_TIME_FOR_THE_PILL, PILL_FROM_THE_BOTTLE);
+        ivrResponse = caller.enter("3");
+        assertAudioFilesPresent(ivrResponse, MISSED_PILL_FEEDBACK_LESS_THAN_70, DOSE_CANNOT_BE_TAKEN_MENU);
+        ivrResponse = caller.enter("2");
+        assertAudioFilesPresent(ivrResponse, PLEASE_CARRY_SMALL_BOX, YOUR_ADHERENCE_IS_NOW, "Num_000", PERCENT);
+    }
+}
