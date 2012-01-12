28325806bfd3de36d722666fb8937d065439b946
Alok Simha <lukasimha@gmail.com>
2012-01-12 09:54:18 +0530

    Luka | #0000 | Moving filtering of alert code to the PatientAlerts collection

--- tama.patient/src/main/java/org/motechproject/tama/patient/domain/PatientAlerts.java
+++ tama.patient/src/main/java/org/motechproject/tama/patient/domain/PatientAlerts.java
@@ -3,6 +3,8 @@ package org.motechproject.tama.patient.domain;
 
 import ch.lambdaj.Lambda;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
+import org.joda.time.DateTime;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -24,5 +26,45 @@ public class PatientAlerts extends ArrayList<PatientAlert> {
         List<PatientAlert> filterCriteria = filter(Lambda.<PatientAlert>having(on(PatientAlert.class).isSymptomReportingAlert()), this);
         List<Object> objectList = sort(filterCriteria, on(PatientAlert.class).getAlert().getDateTime(), reverseOrder());
         return CollectionUtils.isEmpty(objectList) ? null : (PatientAlert) objectList.get(0);
+    }
+
+    public PatientAlerts filterByAlertTypeAndDateRange(PatientAlertType patientAlertType, DateTime startDate, DateTime endDate) {
+        Predicate selectorForAlertTypeAndDateRange = getSelectorForAlertTypeAndDateRange(patientAlertType, startDate, endDate);
+        ArrayList<PatientAlert> filteredAlerts = new ArrayList<PatientAlert>();
+        CollectionUtils.select(this, selectorForAlertTypeAndDateRange, filteredAlerts);
+        return new PatientAlerts(filteredAlerts);
+    }
+
+    public PatientAlerts filterByAlertTypeAndDateRangeIfPresent(PatientAlertType patientAlertType, DateTime startDate, DateTime endDate) {
+        Predicate selectorForAlertTypeAndDateRangeIfPresent = getSelectorForAlertTypeAndDateRangeIfPresent(patientAlertType, startDate, endDate);
+        ArrayList<PatientAlert> filteredAlerts = new ArrayList<PatientAlert>();
+        CollectionUtils.select(this, selectorForAlertTypeAndDateRangeIfPresent, filteredAlerts);
+        return new PatientAlerts(filteredAlerts);
+    }
+
+    private Predicate getSelectorForAlertTypeAndDateRange(final PatientAlertType patientAlertType, final DateTime startDate, final DateTime endDate) {
+        return new Predicate() {
+            @Override
+            public boolean evaluate(Object o) {
+                PatientAlert patientAlert = (PatientAlert) o;
+                DateTime alertTime = patientAlert.getAlert().getDateTime();
+                boolean isOfRequiredAlertType = patientAlert.getAlert().getData() != null && patientAlertType.name().equals(patientAlert.getAlert().getData().get(PatientAlert.PATIENT_ALERT_TYPE));
+                return isOfRequiredAlertType && alertTime.isAfter(startDate) && alertTime.isBefore(endDate);
+            }
+        };
+    }
+
+    private Predicate getSelectorForAlertTypeAndDateRangeIfPresent(final PatientAlertType patientAlertType, final DateTime startDate, final DateTime endDate) {
+        return new Predicate() {
+            @Override
+            public boolean evaluate(Object o) {
+                PatientAlert patientAlert = (PatientAlert) o;
+                DateTime alertTime = patientAlert.getAlert().getDateTime();
+                boolean isOfRequiredAlertType = patientAlertType == null || (patientAlert.getAlert().getData() != null && patientAlertType.name().equals(patientAlert.getAlert().getData().get(PatientAlert.PATIENT_ALERT_TYPE)));
+                boolean isAfterStartDate = startDate == null || alertTime.isAfter(startDate);
+                boolean isBeforeEndDate = endDate == null || !alertTime.toLocalDate().isAfter(endDate.toLocalDate());
+                return isOfRequiredAlertType && isAfterStartDate && isBeforeEndDate;
+            }
+        };
     }
 }
--- tama.patient/src/main/java/org/motechproject/tama/patient/service/PatientAlertService.java
+++ tama.patient/src/main/java/org/motechproject/tama/patient/service/PatientAlertService.java
@@ -1,13 +1,8 @@
 package org.motechproject.tama.patient.service;
 
-import ch.lambdaj.Lambda;
-import ch.lambdaj.function.convert.Converter;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.Predicate;
 import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
 import org.motechproject.server.alerts.domain.Alert;
-import org.motechproject.server.alerts.domain.AlertCriteria;
 import org.motechproject.server.alerts.domain.AlertStatus;
 import org.motechproject.server.alerts.domain.AlertType;
 import org.motechproject.server.alerts.service.AlertService;
@@ -18,9 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
-
-import static ch.lambdaj.Lambda.*;
-import static java.util.Collections.reverseOrder;
 
 @Component
 public class PatientAlertService {
@@ -39,27 +31,6 @@ public class PatientAlertService {
         this.patientAlertSearchService = new PatientAlertSearchService(alertService);
     }
 
-    public PatientAlert readAlert(String alertId) {
-        final Alert alert = alertService.get(alertId);
-        alertService.changeStatus(alert.getId(), AlertStatus.READ);
-        return PatientAlert.newPatientAlert(alert, allPatients.get(alert.getExternalId()));
-    }
-
-    public PatientAlerts getReadAlertsFor(String clinicId, String patientId, PatientAlertType patientAlertType, DateTime startDate, DateTime endDate) {
-        return getAlertsOfSpecificTypeAndStatusAndDateRange(clinicId, patientId, AlertStatus.READ, patientAlertType, startDate, endDate);
-    }
-
-    public PatientAlerts getUnreadAlertsFor(String clinicId, String patientId, PatientAlertType patientAlertType, DateTime startDate, DateTime endDate) {
-        return getAlertsOfSpecificTypeAndStatusAndDateRange(clinicId, patientId, AlertStatus.NEW, patientAlertType, startDate, endDate);
-    }
-
-    public PatientAlerts getAllAlertsBy(final String patientId) {
-        final ArrayList<Patient> patients = new ArrayList<Patient>() {{
-            add(allPatients.get(patientId));
-        }};
-        return patientAlertSearchService.search(patients, null);
-    }
-
     public void createAlert(String externalId, Integer priority, String name, String description, PatientAlertType patientAlertType, Map<String, String> data) {
         data.put(PatientAlert.PATIENT_ALERT_TYPE, patientAlertType.name());
         Patient patient = allPatients.get(externalId);
@@ -70,6 +41,12 @@ public class PatientAlertService {
             data.put(PatientAlert.SYMPTOMS_ALERT_STATUS, SymptomsAlertStatus.Open.name());
         }
         alertService.create(externalId, name, description, AlertType.MEDIUM, AlertStatus.NEW, priority, data);
+    }
+
+    public PatientAlert readAlert(String alertId) {
+        final Alert alert = alertService.get(alertId);
+        alertService.changeStatus(alert.getId(), AlertStatus.READ);
+        return PatientAlert.newPatientAlert(alert, allPatients.get(alert.getExternalId()));
     }
 
     public boolean updateAlert(String alertId, String symptomsAlertStatus, String notes, String doctorsNotes, String patientAlertType) {
@@ -94,15 +71,32 @@ public class PatientAlertService {
         alertService.setData(lastReportedAlert.getAlertId(), PatientAlert.DOCTOR_NAME, doctorName);
     }
 
+    public PatientAlerts getReadAlertsFor(String clinicId, String patientId, PatientAlertType patientAlertType, DateTime startDate, DateTime endDate) {
+        return getAlertsOfSpecificTypeAndStatusAndDateRange(clinicId, patientId, AlertStatus.READ, patientAlertType, startDate, endDate);
+    }
+
+    public PatientAlerts getUnreadAlertsFor(String clinicId, String patientId, PatientAlertType patientAlertType, DateTime startDate, DateTime endDate) {
+        return getAlertsOfSpecificTypeAndStatusAndDateRange(clinicId, patientId, AlertStatus.NEW, patientAlertType, startDate, endDate);
+    }
+
     public PatientAlerts getFallingAdherenceAlerts(String patientID, final DateTime startDate, final DateTime endDate) {
-        return getAlertsOfSpecificTypeAndForDateRange(patientID, PatientAlertType.FallingAdherence, startDate, endDate);
+        PatientAlerts allAlerts = getAllAlertsBy(patientID);
+        return allAlerts.filterByAlertTypeAndDateRange(PatientAlertType.FallingAdherence, startDate, endDate);
     }
 
     public PatientAlerts getAdherenceInRedAlerts(String patientID, final DateTime startDate, final DateTime endDate) {
-        return getAlertsOfSpecificTypeAndForDateRange(patientID, PatientAlertType.AdherenceInRed, startDate, endDate);
+        PatientAlerts allAlerts = getAllAlertsBy(patientID);
+        return allAlerts.filterByAlertTypeAndDateRange(PatientAlertType.AdherenceInRed, startDate, endDate);
     }
 
-    public PatientAlerts getAlertsOfSpecificTypeAndStatusAndDateRange(String clinicId, String patientId, AlertStatus alertStatus, PatientAlertType patientAlertType, DateTime startDate, DateTime endDate) {
+    PatientAlerts getAllAlertsBy(final String patientId) {
+        final ArrayList<Patient> patients = new ArrayList<Patient>() {{
+            add(allPatients.get(patientId));
+        }};
+        return patientAlertSearchService.search(patients, null);
+    }
+
+    PatientAlerts getAlertsOfSpecificTypeAndStatusAndDateRange(String clinicId, String patientId, AlertStatus alertStatus, PatientAlertType patientAlertType, DateTime startDate, DateTime endDate) {
         List<Patient> patients = Collections.emptyList();
         if (patientId != null) {
             Patient patient = allPatients.findByPatientIdAndClinicId(patientId, clinicId);
@@ -113,45 +107,6 @@ public class PatientAlertService {
             patients = allPatients.findByClinic(clinicId);
         }
         PatientAlerts allAlerts = patientAlertSearchService.search(patients, alertStatus);
-        Predicate selectorForAlertTypeAndDateRange = getSelectorForAlertTypeAndDateRangeIfPresent(patientAlertType, startDate, endDate);
-        return filterAlertsByPredicate(allAlerts, selectorForAlertTypeAndDateRange);
-    }
-
-    private PatientAlerts getAlertsOfSpecificTypeAndForDateRange(String patientID, PatientAlertType patientAlertType, DateTime startDate, DateTime endDate) {
-        PatientAlerts allAlerts = getAllAlertsBy(patientID);
-        Predicate selectorForAlertTypeAndDateRange = getSelectorForAlertTypeAndDateRange(patientAlertType, startDate, endDate);
-        return filterAlertsByPredicate(allAlerts, selectorForAlertTypeAndDateRange);
-    }
-
-    private PatientAlerts filterAlertsByPredicate(PatientAlerts allAlerts, Predicate predicate) {
-        ArrayList<PatientAlert> filteredAlerts = new ArrayList<PatientAlert>();
-        CollectionUtils.select(allAlerts, predicate, filteredAlerts);
-        return new PatientAlerts(filteredAlerts);
-    }
-
-    private Predicate getSelectorForAlertTypeAndDateRangeIfPresent(final PatientAlertType patientAlertType, final DateTime startDate, final DateTime endDate) {
-        return new Predicate() {
-            @Override
-            public boolean evaluate(Object o) {
-                PatientAlert patientAlert = (PatientAlert) o;
-                DateTime alertTime = patientAlert.getAlert().getDateTime();
-                boolean isOfRequiredAlertType = patientAlertType == null || (patientAlert.getAlert().getData() != null && patientAlertType.name().equals(patientAlert.getAlert().getData().get(PatientAlert.PATIENT_ALERT_TYPE)));
-                boolean isAfterStartDate = startDate == null || alertTime.isAfter(startDate);
-                boolean isBeforeEndDate = endDate == null || !alertTime.toLocalDate().isAfter(endDate.toLocalDate());
-                return isOfRequiredAlertType && isAfterStartDate && isBeforeEndDate;
-            }
-        };
-    }
-
-    private Predicate getSelectorForAlertTypeAndDateRange(final PatientAlertType patientAlertType, final DateTime startDate, final DateTime endDate) {
-        return new Predicate() {
-            @Override
-            public boolean evaluate(Object o) {
-                PatientAlert patientAlert = (PatientAlert) o;
-                DateTime alertTime = patientAlert.getAlert().getDateTime();
-                boolean isOfRequiredAlertType = patientAlert.getAlert().getData() != null && patientAlertType.name().equals(patientAlert.getAlert().getData().get(PatientAlert.PATIENT_ALERT_TYPE));
-                return isOfRequiredAlertType && alertTime.isAfter(startDate) && alertTime.isBefore(endDate);
-            }
-        };
+        return allAlerts.filterByAlertTypeAndDateRangeIfPresent(patientAlertType, startDate, endDate);
     }
 }
