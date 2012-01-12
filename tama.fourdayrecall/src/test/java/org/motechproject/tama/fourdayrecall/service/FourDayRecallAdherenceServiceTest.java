b1297406b228b417a707e0683a87814d12af1461
Balaji Narain <narainbalaji@gmail.com>
2012-01-12 18:42:25 +0530

    Sharvani, BalajiN | #1083 | Adding basic functional test for FourDayRecall

--- tama.fourdayrecall/src/test/java/org/motechproject/tama/fourdayrecall/service/FourDayRecallAdherenceServiceTest.java
+++ tama.fourdayrecall/src/test/java/org/motechproject/tama/fourdayrecall/service/FourDayRecallAdherenceServiceTest.java
@@ -3,20 +3,39 @@ package org.motechproject.tama.fourdayrecall.service;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.motechproject.tama.fourdayrecall.domain.WeeklyAdherenceLog;
 import org.motechproject.tama.ivr.service.AdherenceService;
 import org.motechproject.testing.utils.BaseUnitTest;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class FourDayRecallAdherenceServiceTest extends BaseUnitTest {
 
     @Mock
-    private AdherenceService adherenceService;
+    AdherenceService adherenceService;
+    FourDayRecallDateService fourDayRecallDateService;
+    FourDayRecallAdherenceService fourDayRecallAdherenceService;
 
     @Before
     public void setUp() {
         initMocks(this);
+        fourDayRecallDateService = new FourDayRecallDateService();
+        fourDayRecallAdherenceService = new FourDayRecallAdherenceService(null, null, null, fourDayRecallDateService, adherenceService);
+    }
+
+    @Test
+    public void shouldReturn100PercentWhenNoDosesMissed() {
+        assertEquals(100, fourDayRecallAdherenceService.adherencePercentageFor(0));
+    }
+
+    @Test
+    public void shouldReturnAdherencePercentage() {
+        int numberOfDosesMissed = 2;
+        int numberOfDosesSupposedToTake = 4;
+        int adherence = (numberOfDosesMissed * 100) / numberOfDosesSupposedToTake;
+        assertEquals(adherence, fourDayRecallAdherenceService.adherencePercentageFor(numberOfDosesMissed));
     }
 
     @Test
---
+++ tama.web/src/test/java/org/motechproject/tamafunctional/test/fourdayrecall/FourDayRecallTest.java
@@ -0,0 +1,80 @@
+package org.motechproject.tamafunctional.test.fourdayrecall;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.junit.Before;
+import org.junit.Test;
+import org.motechproject.tamadatasetup.service.TAMADateTimeService;
+import org.motechproject.tamafunctional.test.ivr.BaseIVRTest;
+import org.motechproject.tamafunctional.test.ivr.IVRAssert;
+import org.motechproject.tamafunctional.testdata.FourDayRecallCallInfo;
+import org.motechproject.tamafunctional.testdata.TestClinician;
+import org.motechproject.tamafunctional.testdata.TestPatient;
+import org.motechproject.tamafunctional.testdata.TestPatientPreferences;
+import org.motechproject.tamafunctional.testdata.ivrreponse.IVRResponse;
+import org.motechproject.tamafunctional.testdata.treatmentadvice.TestDrugDosage;
+import org.motechproject.tamafunctional.testdata.treatmentadvice.TestTreatmentAdvice;
+import org.motechproject.tamafunctional.testdataservice.PatientDataService;
+
+import static org.motechproject.tama.ivr.TamaIVRMessage.*;
+
+public class FourDayRecallTest extends BaseIVRTest {
+    private TAMADateTimeService tamaDateTimeService;
+
+    private TestPatient patient;
+    private TestClinician clinician;
+
+    private DateTime now = new DateTime(2012, 1, 1, 10, 0, 0, 0);
+    private DateTime registrationTime = now.minusDays(7);
+
+    public FourDayRecallTest() {
+        super.setUp();
+        tamaDateTimeService = new TAMADateTimeService(webClient);
+    }
+
+    @Before
+    public void setUp() {
+        caller = caller(patient);
+    }
+
+    @Before
+    public void createTreatmentAdvice() {
+        PatientDataService patientDataService = new PatientDataService(webDriver);
+        TestTreatmentAdvice treatmentAdvice = setUpTreatmentAdviceOn(registrationTime.toLocalDate());
+        patientDataService.setupARTRegimenWithDependents(treatmentAdvice, patient, clinician);
+    }
+
+    private TestTreatmentAdvice setUpTreatmentAdviceOn(LocalDate treatmentAdviceStartDate) {
+        TestDrugDosage[] drugDosages = TestDrugDosage.create("Efferven", "Combivir");
+        drugDosages[0].startDate(treatmentAdviceStartDate);
+        drugDosages[1].startDate(treatmentAdviceStartDate);
+        return TestTreatmentAdvice.withExtrinsic(drugDosages);
+    }
+
+    @Before
+    public void registerPatient() {
+        tamaDateTimeService.adjustDateTime(registrationTime);
+        clinician = TestClinician.withMandatory();
+        TestPatientPreferences preferences = TestPatientPreferences.withMandatory().callPreference(TestPatientPreferences.CallPreference.WEEKLY_CALL).dayOfWeeklyCall("Monday").bestCallTime("10:10");
+        patient = TestPatient.withMandatory().patientPreferences(preferences);
+    }
+
+    @Test
+    public void shouldReportAdherenceOnBestCallDay() {
+        tamaDateTimeService.adjustDateTime(now);
+        caller.replyToCall(new FourDayRecallCallInfo());
+        IVRResponse ivrResponse = caller.enter(patient.patientPreferences().passcode());
+        ivrResponse = enterAdherenceInformation(ivrResponse, "0");
+        assertAdherencePercentage(ivrResponse, "100");
+    }
+
+    private IVRResponse enterAdherenceInformation(IVRResponse ivrResponse, String keyPressed) {
+        IVRAssert.asksForCollectDtmfWith(ivrResponse, DEFAULT_OUTBOUND_CLINIC_MESSAGE, FDR_GREETING, FDR_MENU_FOR_SINGLE_DOSAGE);
+        return caller.enter(keyPressed);
+    }
+
+    private void assertAdherencePercentage(IVRResponse ivrResponse, String expectedAdherence) {
+        IVRAssert.assertAudioFilesPresent(ivrResponse, FDR_YOUR_WEEKLY_ADHERENCE_IS, "Num_" + expectedAdherence, FDR_PERCENT);
+        caller.hangup();
+    }
+}
