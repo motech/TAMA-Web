fc739f25e12fd697fe61336a8a9b5413e36630a4
shruthi <shruthidipali@gmail.com>
2012-01-19 15:21:19 +0530

    Sharvani, Shruthi | #0000 | Using total logs to determine number of missed doses to play the missed pill feedback.

--- tama.dailypillreminder/src/test/java/org/motechproject/tama/dailypillreminder/domain/PillRegimenTest.java
+++ tama.dailypillreminder/src/test/java/org/motechproject/tama/dailypillreminder/domain/PillRegimenTest.java
@@ -287,44 +287,6 @@ public class PillRegimenTest extends BaseUnitTest {
     }
 
     @Test
-    public void dosagesForLastFourWeeksShouldBe28ForSingleDoseRegimen() {
-        final DateTime dosageStartDate = new DateTime(2011, 10, 2, 10, 0, 0, 0);
-        PillRegimenResponse pillRegimenResponse = new PillRegimenResponse("regimen_id", "patient_id", 2, 15, new ArrayList<DosageResponse>() {{
-            add(new DosageResponse("dosage_id", new Time(dosageStartDate.getHourOfDay(), dosageStartDate.getMinuteOfHour()), dosageStartDate.toLocalDate(), null, null, null));
-        }});
-        PillRegimen pillRegimen = new PillRegimen(pillRegimenResponse);
-
-        DateTime today = dosageStartDate.plusWeeks(4).minusDays(1);
-        assertEquals(28, pillRegimen.getDosesBetween(dosageStartDate.toLocalDate(), today));
-    }
-
-    @Test
-    public void dosagesShouldBe28ForSingleDoseRegimenStarting4WeeksBack() {
-        final DateTime dosageStartDate = new DateTime(2011, 10, 2, 10, 0, 0, 0);
-        PillRegimenResponse pillRegimenResponse = new PillRegimenResponse("regimen_id", "patient_id", 2, 15, new ArrayList<DosageResponse>() {{
-            add(new DosageResponse("dosage_id", new Time(dosageStartDate.getHourOfDay(), dosageStartDate.getMinuteOfHour()), dosageStartDate.toLocalDate(), null, null, null));
-        }});
-        PillRegimen pillRegimen = new PillRegimen(pillRegimenResponse);
-
-        DateTime today = dosageStartDate.plusWeeks(4).minusDays(1);
-        assertEquals(28, pillRegimen.getNumberOfDosesAsOf(today));
-    }
-
-    @Test
-    public void dosagesForLastFourWeeksShouldBe51ForTwoDoseRegimen() {
-        final DateTime dosage1StartDate = new DateTime(2011, 10, 2, 10, 0, 0, 0);
-        final DateTime dosage2StartDate = new DateTime(2011, 10, 7, 13, 0, 0, 0);
-        final DateTime today = new DateTime(2011, 10, 30, 13, 0, 0, 0);
-        PillRegimenResponse pillRegimenResponse = new PillRegimenResponse("regimen_id", "patient_id", 2, 15, new ArrayList<DosageResponse>() {{
-            add(new DosageResponse("dosage1_id", new Time(dosage1StartDate.getHourOfDay(), dosage1StartDate.getMinuteOfHour()), dosage1StartDate.toLocalDate(), null, null, null));
-            add(new DosageResponse("dosage2_id", new Time(dosage2StartDate.getHourOfDay(), dosage2StartDate.getMinuteOfHour()), dosage2StartDate.toLocalDate(), null, null, null));
-        }});
-        PillRegimen pillRegimen = new PillRegimen(pillRegimenResponse);
-
-        assertEquals(51, pillRegimen.getDosesBetween(dosage1StartDate.toLocalDate(), today.minusDays(1)));
-    }
-
-    @Test
     public void shouldReturnTodaysDoseWhenPatientIsOnSingleDosage_AndTimeSpecifiedIsAfterPillTime() {
         final DateTime dosage1StartDate = new DateTime(2011, 10, 2, 10, 30, 0, 0);
         PillRegimenResponse pillRegimenResponse = new PillRegimenResponse("regimen_id", "patient_id", 1, 0, new ArrayList<DosageResponse>() {{
