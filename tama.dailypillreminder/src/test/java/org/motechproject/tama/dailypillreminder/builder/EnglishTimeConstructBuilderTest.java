3826ffeaa34070bffecd3c7319fa9019ba4ed3f9
Sharvani <sharvani.n.s@thoughtworks.com>
2012-01-31 10:59:25 +0530

    Sharvani, Luka | #718 | Added tests to test slots for TamilTimeConstructBuilder

--- tama.dailypillreminder/src/test/java/org/motechproject/tama/dailypillreminder/builder/EnglishTimeConstructBuilderTest.java
+++ tama.dailypillreminder/src/test/java/org/motechproject/tama/dailypillreminder/builder/EnglishTimeConstructBuilderTest.java
@@ -2,19 +2,18 @@ package org.motechproject.tama.dailypillreminder.builder;
 
 import org.joda.time.LocalTime;
 import org.junit.Test;
-import org.motechproject.tama.dailypillreminder.builder.timeconstruct.TimeConstructBuilder;
-import org.motechproject.tama.refdata.domain.IVRLanguage;
+import org.motechproject.tama.dailypillreminder.builder.timeconstruct.EnglishTimeConstructBuilder;
 
 import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
 
-public class TimeConstructBuilder_ForEnglish_Test {
+public class EnglishTimeConstructBuilderTest {
 
     @Test
     public void shouldCreateTimeConstruct_AtTheExactHour() {
         LocalTime tenAM = new LocalTime(10, 0, 0);
-        List<String> timeConstructWAVs = new TimeConstructBuilder().build(IVRLanguage.ENGLISH_CODE, tenAM);
+        List<String> timeConstructWAVs = new EnglishTimeConstructBuilder().build(tenAM);
         assertEquals(2, timeConstructWAVs.size());
         assertEquals("Num_010", timeConstructWAVs.get(0));
         assertEquals("timeofDayAM", timeConstructWAVs.get(1));
@@ -23,7 +22,7 @@ public class TimeConstructBuilder_ForEnglish_Test {
     @Test
     public void shouldCreateTimeConstruct_AtSomeMinutesPastAnHour() {
         LocalTime ten30AM = new LocalTime(10, 30, 0);
-        List<String> timeConstructWAVs = new TimeConstructBuilder().build(IVRLanguage.ENGLISH_CODE, ten30AM);
+        List<String> timeConstructWAVs = new EnglishTimeConstructBuilder().build(ten30AM);
         assertEquals(3, timeConstructWAVs.size());
         assertEquals("Num_010", timeConstructWAVs.get(0));
         assertEquals("Num_030", timeConstructWAVs.get(1));
@@ -33,7 +32,7 @@ public class TimeConstructBuilder_ForEnglish_Test {
     @Test
     public void shouldCreateTimeConstruct_AtMidnight() {
         LocalTime midnight = new LocalTime(0, 0, 0);
-        List<String> timeConstructWAVs = new TimeConstructBuilder().build(IVRLanguage.ENGLISH_CODE, midnight);
+        List<String> timeConstructWAVs = new EnglishTimeConstructBuilder().build(midnight);
         assertEquals(2, timeConstructWAVs.size());
         assertEquals("Num_012", timeConstructWAVs.get(0));
         assertEquals("timeofDayAM", timeConstructWAVs.get(1));
@@ -42,7 +41,7 @@ public class TimeConstructBuilder_ForEnglish_Test {
     @Test
     public void shouldCreateTimeConstruct_AtNoon() {
         LocalTime noon = new LocalTime(12, 0, 0);
-        List<String> timeConstructWAVs = new TimeConstructBuilder().build(IVRLanguage.ENGLISH_CODE, noon);
+        List<String> timeConstructWAVs = new EnglishTimeConstructBuilder().build(noon);
         assertEquals(2, timeConstructWAVs.size());
         assertEquals("Num_012", timeConstructWAVs.get(0));
         assertEquals("timeofDayPM", timeConstructWAVs.get(1));
---
+++ tama.dailypillreminder/src/test/java/org/motechproject/tama/dailypillreminder/builder/timeconstruct/TimeConstructBuilderTest.java
@@ -0,0 +1,43 @@
+package org.motechproject.tama.dailypillreminder.builder.timeconstruct;
+
+import org.junit.Test;
+import org.motechproject.tama.refdata.domain.IVRLanguage;
+
+import static junit.framework.Assert.assertEquals;
+
+public class TimeConstructBuilderTest {
+
+    @Test
+    public void constructEnglishTimeConstructBuilder() {
+        SlotBasedTimeConstructBuilder builder = new TimeConstructBuilder().builder(IVRLanguage.ENGLISH_CODE);
+        assertEquals(EnglishTimeConstructBuilder.class, builder.getClass());
+    }
+
+    @Test
+    public void constructMarathiTimeConstructBuilder() {
+        SlotBasedTimeConstructBuilder builder = new TimeConstructBuilder().builder(IVRLanguage.MARATHI_CODE);
+        assertEquals(MarathiTimeConstructBuilder.class, builder.getClass());
+    }
+
+    @Test
+    public void constructHindiTimeConstructBuilder() {
+        SlotBasedTimeConstructBuilder builder = new TimeConstructBuilder().builder(IVRLanguage.HINDI_CODE);
+        assertEquals(HindiTimeConstructBuilder.class, builder.getClass());
+    }
+
+    @Test
+    public void constructTamilTimeConstructBuilder() {
+        SlotBasedTimeConstructBuilder builder = new TimeConstructBuilder().builder(IVRLanguage.TAMIL_CODE);
+        assertEquals(TamilTimeConstructBuilder.class, builder.getClass());
+    }
+
+    @Test
+    public void constructTimeConstructBuilder_ForNotSupportedLanguage() {
+        try {
+            new TimeConstructBuilder().builder("unknown");
+        }
+        catch(Exception e) {
+            assertEquals("TimeConstruct for Preferred Language - unknown not supported", e.getMessage());
+        }
+    }
+}
