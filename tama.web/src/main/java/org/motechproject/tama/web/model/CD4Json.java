b712c8ccd14714112fcdff6ce1819ff02ee5b451
Preethi <preethid@thoughtworks.com>
2012-02-22 16:19:50 +0530

    Sharvani, Preethi | #1253 | CD4 time graph modified to show last 3 years of data.

--- tama.web/src/main/java/org/motechproject/tama/web/model/CD4Json.java
+++ tama.web/src/main/java/org/motechproject/tama/web/model/CD4Json.java
@@ -3,6 +3,7 @@ package org.motechproject.tama.web.model;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.motechproject.tama.common.TAMAConstants;
 import org.motechproject.tama.patient.domain.LabResult;
 
 import java.util.List;
@@ -17,7 +18,7 @@ public class CD4Json extends JSONArray {
 
     private JSONObject createResult(LabResult labResult) throws JSONException {
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("date", labResult.getTestDate());
+        jsonObject.put("date", labResult.getTestDate().toString(TAMAConstants.DATE_FORMAT));
         jsonObject.put("value", labResult.getResult());
         return jsonObject;
     }
