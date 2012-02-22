9e010c3f88cf053915117e43ecd76cb1891e7a98
shruthi <shruthidipali@gmail.com>
2012-02-22 15:51:23 +0530

    BalajiN, Shruthi | #1248 | Visit Date can now be in the past. Changed SchedulePopup to accept a parameter to disable dates in the past.

--- tama.web/src/main/webapp/js/schedulePopup.js
+++ tama.web/src/main/webapp/js/schedulePopup.js
@@ -2,42 +2,56 @@ dojo.require("dojo.parser");
 dojo.require("dijit.TooltipDialog");
 dojo.require("dijit.form.TimeTextBox");
 
-var SchedulePopup = function(id, titleText){
-    this.id=id;
+var SchedulePopup = function(id, titleText) {
+    this.id = id;
     var calendar = new dijit.Calendar({
-                        value: new Date(),
-                        isDisabledDate: function(d){
-                            var startDate = new Date();
-                            startDate.setDate(startDate.getDate() - 1);
-                           return startDate.getTime() > d.getTime();
-                        }
-                    }, dojo.query(".calendar", id)[0]);
+        value: new Date()
+    }, dojo.query(".calendar", id)[0]);
 
     var timebox = new dijit.form.TimeTextBox({name:"timePicker", value:new Date(),
         constraints:{timePattern:'HH:mm:ss', clickableIncrement:'T00:15:00', visibleIncrement:'T00:15:00', visibleRange:'T01:00:00'}
-      }, dojo.query(".timePicker", id)[0]);
+    }, dojo.query(".timePicker", id)[0]);
 
     var adjustDueDateDlg = new dijit.TooltipDialog({
-                        title: titleText,
-                        style: "",
-                        content: dojo.byId(id)
-                    });
+        title: titleText,
+        style: "",
+        content: dojo.byId(id)
+    });
     var okHandlerConnect = null;
     var cancelHandlerConnect = null;
 
+    var futureDates = function(d) {
+        var startDate = new Date();
+        startDate.setDate(startDate.getDate() - 1);
+        return startDate.getTime() > d.getTime();
+    }
+
+    var allDates = function(d) {
+        return false;
+    }
+
 
     this.show = function(element, showOptions /*bool*/, okHandler) {
-        var showTimeOption =  showOptions && showOptions['showTimeOption'];
-        var showVisitOptions =  showOptions && showOptions['showVisitOptions'];
-        dijit.popup.open({ popup: adjustDueDateDlg,around: element });
+        var showTimeOption = showOptions && showOptions['showTimeOption'];
+        var showVisitOptions = showOptions && showOptions['showVisitOptions'];
+        var disablePastDates = showOptions && showOptions['disablePastDates'];
 
-        dojo.query('.timePickerDiv', id).style('display', showTimeOption ?'':'none');
-        dojo.query('.visitTypeOptions', id).style('display', showVisitOptions ?'':'none');
+        var isDisabledDate = null;
+        if (disablePastDates === false) {
+            calendar.isDisabledDate = allDates;
+        } else {
+            calendar.isDisabledDate = futureDates;
+        }
+        calendar._populateGrid();
 
+        dijit.popup.open({ popup: adjustDueDateDlg, around: element });
+
+        dojo.query('.timePickerDiv', id).style('display', showTimeOption ? '' : 'none');
+        dojo.query('.visitTypeOptions', id).style('display', showVisitOptions ? '' : 'none');
 
 
         if (cancelHandlerConnect == null) {
-            cancelHandlerConnect  = dojo.query('.popupCancel',id).connect('onclick', function(){
+            cancelHandlerConnect = dojo.query('.popupCancel', id).connect('onclick', function() {
                 dijit.popup.close(adjustDueDateDlg);
             });
         }
@@ -45,19 +59,19 @@ var SchedulePopup = function(id, titleText){
 
         if (okHandlerConnect != null) dojo.disconnect(okHandlerConnect);
 
-        okHandlerConnect = dojo.connect(dojo.query('.popupOk',id)[0], 'onclick',
-                function(){
-                    var dateValue = calendar.value;
-                    if (showTimeOption){
-                        dateValue.setHours(timebox.value.getHours());
-                        dateValue.getMinutes(timebox.value.getMinutes());
-                    }
-                    var optionScheduled;
-                    if (showVisitOptions) {
-                        optionScheduled = {"visitOptions" : {"optionScheduled" :  dojo.query(".optionScheduled")[0].checked }};
-                    }
-                    okHandler(dateValue, optionScheduled);
-                    dijit.popup.close(adjustDueDateDlg);
-                });
+        okHandlerConnect = dojo.connect(dojo.query('.popupOk', id)[0], 'onclick',
+            function() {
+                var dateValue = calendar.value;
+                if (showTimeOption) {
+                    dateValue.setHours(timebox.value.getHours());
+                    dateValue.getMinutes(timebox.value.getMinutes());
+                }
+                var optionScheduled;
+                if (showVisitOptions) {
+                    optionScheduled = {"visitOptions" : {"optionScheduled" :  dojo.query(".optionScheduled")[0].checked }};
+                }
+                okHandler(dateValue, optionScheduled);
+                dijit.popup.close(adjustDueDateDlg);
+            });
     }
 };
