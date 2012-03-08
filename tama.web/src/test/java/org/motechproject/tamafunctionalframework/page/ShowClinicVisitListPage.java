28150df52b4409649c027519d796e22b9c9d7b3d
Vinkesh Banka <vinkeshb@thoughtworks.com>
2012-03-08 12:49:22 +0530

    Banka | #000 | Added clinic visit regression tests.

--- tama.web/src/test/java/org/motechproject/tamafunctionalframework/page/ShowClinicVisitListPage.java
+++ tama.web/src/test/java/org/motechproject/tamafunctionalframework/page/ShowClinicVisitListPage.java
@@ -1,6 +1,8 @@
 package org.motechproject.tamafunctionalframework.page;
 
+import org.motechproject.tama.common.TAMAConstants;
 import org.motechproject.tamafunctionalframework.framework.MyPageFactory;
+import org.motechproject.util.DateUtil;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -12,6 +14,21 @@ public class ShowClinicVisitListPage extends Page {
 
     @FindBy(how = How.ID, using = "visit-0")
     private WebElement firstVisitLink;
+
+    @FindBy(how = How.XPATH, using="//tr[@visitid='week4']//a[@class='adjustDueDateAsToday']")
+    WebElement adjustDueDateAsTodayLink;
+
+    @FindBy(how = How.XPATH, using="//tr[@visitid='week4']//a[@class='adjustDueDate']")
+    WebElement adjustDueDateLink;
+
+    @FindBy(how = How.XPATH, using="//tr[@visitid='week4']//a[@class='markAsMissed']")
+    WebElement markAsMissedLink;
+
+    @FindBy(how = How.XPATH, using="//tr[@visitid='week4']//td[@class='visitDateColumn']")
+    WebElement visitDateColumn;
+
+    @FindBy(how = How.ID, using="confirmok")
+    WebElement confirmok;
 
     public ShowClinicVisitListPage(WebDriver webDriver) {
         super(webDriver);
@@ -41,5 +58,25 @@ public class ShowClinicVisitListPage extends Page {
 
     public String getFirstVisitDescription() {
         return firstVisitLink.getText();
+    }
+
+    public void adjustDueDateAsToday(){
+        adjustDueDateAsTodayLink.click();
+        String today = DateUtil.today().toString(TAMAConstants.DATE_FORMAT);
+        waitForElementWithXPATHToLoad("//tr[@visitid='week4']//td[@class='adjustDueDateColumn' and a='" + today + "']");
+    }
+
+    public String getAdjustedDueDate(){
+        return adjustDueDateLink.getText();
+    }
+
+    public void markAsMissed(){
+        markAsMissedLink.click();
+        confirmok.click();
+        waitForElementWithXPATHToLoad("//tr[@visitid='week4' and td='Missed']");
+    }
+
+    public String getVisitDate(){
+        return visitDateColumn.getText();
     }
 }
