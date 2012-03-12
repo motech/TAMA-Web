7de79a44d72cb09f62e9c4e9bdb6dfebf0b7f377
Srivathsava <srivathr@thoughtworks.com>
2012-03-12 11:41:06 +0530

    <Sri, Dileep> #1360 | Removing DateTime field from ChangePasswordEvent to prevent it from overriding the baseclass dateTime property.

--- tama.web/src/main/java/org/motechproject/tama/security/domain/ChangePasswordEvent.java
+++ tama.web/src/main/java/org/motechproject/tama/security/domain/ChangePasswordEvent.java
@@ -3,7 +3,6 @@ package org.motechproject.tama.security.domain;
 import lombok.Getter;
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.annotate.JsonTypeInfo;
-import org.joda.time.DateTime;
 import org.motechproject.util.DateUtil;
 
 @JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY, property="@class")
@@ -13,7 +12,6 @@ public class ChangePasswordEvent extends TAMAEvent {
     @JsonProperty @Getter String clinicName;
     @JsonProperty @Getter String clinicId;
     @JsonProperty @Getter String username;
-    @JsonProperty @Getter DateTime dateTime;
 
     public ChangePasswordEvent() {}
 
--- tama.web/src/test/java/org/motechproject/tama/security/repository/AllTAMAEventsTest.java
+++ tama.web/src/test/java/org/motechproject/tama/security/repository/AllTAMAEventsTest.java
@@ -8,8 +8,6 @@ import org.motechproject.tama.security.domain.ChangePasswordEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
-import java.util.Arrays;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
