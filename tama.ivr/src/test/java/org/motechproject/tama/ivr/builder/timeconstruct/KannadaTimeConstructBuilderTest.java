ac2c97540d28da4955130efeae02f3853de18c1d
lukasimha <lukasimha@gmail.com>
2012-04-09 20:08:30 +0530

    Luka, Sri | #750 | Changed the Kannada time construct based on the production scripts

--- tama.ivr/src/test/java/org/motechproject/tama/ivr/builder/timeconstruct/KannadaTimeConstructBuilderTest.java
+++ tama.ivr/src/test/java/org/motechproject/tama/ivr/builder/timeconstruct/KannadaTimeConstructBuilderTest.java
@@ -37,7 +37,7 @@ public class KannadaTimeConstructBuilderTest {
     // 3:45:00 - Early Morning Start
     public void shouldCreateTimeConstruct_AtEarlyMorning_Start() {
         LocalTime earlyMorning = new LocalTime(3, 45, 0);
-        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(midnight);
+        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(earlyMorning);
         assertEquals(5, timeConstructWAVs.size());
         assertEquals("timeofDayEarlyMorning", timeConstructWAVs.get(0));
         assertEquals("Num_003", timeConstructWAVs.get(1));
@@ -50,7 +50,7 @@ public class KannadaTimeConstructBuilderTest {
     // 5:59:59 - Early Morning End
     public void shouldCreateTimeConstruct_AtEarlyMorning_End() {
         LocalTime earlyMorning = new LocalTime(5, 59, 59);
-        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(midnight);
+        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(earlyMorning);
         assertEquals(5, timeConstructWAVs.size());
         assertEquals("timeofDayEarlyMorning", timeConstructWAVs.get(0));
         assertEquals("Num_005", timeConstructWAVs.get(1));
@@ -63,7 +63,7 @@ public class KannadaTimeConstructBuilderTest {
     // 6:0:0 - Morning Start
     public void shouldCreateTimeConstruct_AtMorning_Start() {
         LocalTime morning = new LocalTime(6, 0, 0);
-        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(midnight);
+        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(morning);
         assertEquals(3, timeConstructWAVs.size());
         assertEquals("timeofDayMorning", timeConstructWAVs.get(0));
         assertEquals("Num_006", timeConstructWAVs.get(1));
@@ -74,7 +74,7 @@ public class KannadaTimeConstructBuilderTest {
     // 11:59:59 - Morning End
     public void shouldCreateTimeConstruct_AtMorning_End() {
         LocalTime morning = new LocalTime(11, 59, 59);
-        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(midnight);
+        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(morning);
         assertEquals(5, timeConstructWAVs.size());
         assertEquals("timeofDayMorning", timeConstructWAVs.get(0));
         assertEquals("Num_011", timeConstructWAVs.get(1));
@@ -87,7 +87,7 @@ public class KannadaTimeConstructBuilderTest {
     // 12:0:0 - Noon Start
     public void shouldCreateTimeConstruct_AtNoon_Start() {
         LocalTime noon = new LocalTime(12, 0, 0);
-        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(midnight);
+        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(noon);
         assertEquals(3, timeConstructWAVs.size());
         assertEquals("timeOfDayAfternoon", timeConstructWAVs.get(0));
         assertEquals("Num_012", timeConstructWAVs.get(1));
@@ -98,7 +98,7 @@ public class KannadaTimeConstructBuilderTest {
     // 16:44:59 - Noon End
     public void shouldCreateTimeConstruct_AtNoon_End() {
         LocalTime noon = new LocalTime(16, 44, 59);
-        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(midnight);
+        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(noon);
         assertEquals(5, timeConstructWAVs.size());
         assertEquals("timeOfDayAfternoon", timeConstructWAVs.get(0));
         assertEquals("Num_004", timeConstructWAVs.get(1));
@@ -111,7 +111,7 @@ public class KannadaTimeConstructBuilderTest {
     // 16:45:0 - Evening Start
     public void shouldCreateTimeConstruct_AtEvening_Start() {
         LocalTime evening = new LocalTime(16, 45, 0);
-        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(midnight);
+        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(evening);
         assertEquals(5, timeConstructWAVs.size());
         assertEquals("timeOfDayEvening", timeConstructWAVs.get(0));
         assertEquals("Num_004", timeConstructWAVs.get(1));
@@ -124,7 +124,7 @@ public class KannadaTimeConstructBuilderTest {
     // 19:44:59 - Evening End
     public void shouldCreateTimeConstruct_AtEvening_End() {
         LocalTime evening = new LocalTime(19, 44, 59);
-        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(midnight);
+        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(evening);
         assertEquals(5, timeConstructWAVs.size());
         assertEquals("timeOfDayEvening", timeConstructWAVs.get(0));
         assertEquals("Num_007", timeConstructWAVs.get(1));
@@ -137,7 +137,7 @@ public class KannadaTimeConstructBuilderTest {
     // 19:45:0 - Night Start
     public void shouldCreateTimeConstruct_AtNight_Start() {
         LocalTime night = new LocalTime(19, 45, 0);
-        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(midnight);
+        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(night);
         assertEquals(5, timeConstructWAVs.size());
         assertEquals("timeOfDayNight", timeConstructWAVs.get(0));
         assertEquals("Num_007", timeConstructWAVs.get(1));
@@ -150,7 +150,7 @@ public class KannadaTimeConstructBuilderTest {
     // 23:59:59 - Night End
     public void shouldCreateTimeConstruct_AtNight_End() {
         LocalTime night = new LocalTime(23, 59, 59);
-        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(midnight);
+        List<String> timeConstructWAVs = new KannadaTimeConstructBuilder().build(night);
         assertEquals(5, timeConstructWAVs.size());
         assertEquals("timeOfDayNight", timeConstructWAVs.get(0));
         assertEquals("Num_011", timeConstructWAVs.get(1));
