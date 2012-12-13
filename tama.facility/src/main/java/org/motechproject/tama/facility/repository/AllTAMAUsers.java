cc3c3ea0e2f7bfc7fd5a95a1473fd162af972e18
Balaji Narain <balajin@thoughtworks.com>
2012-12-13 12:46:39 +0530

    BalajiN | #272 | Implementing change password for analysts

--- tama.facility/src/main/java/org/motechproject/tama/facility/repository/AllTAMAUsers.java
+++ tama.facility/src/main/java/org/motechproject/tama/facility/repository/AllTAMAUsers.java
@@ -4,7 +4,9 @@ package org.motechproject.tama.facility.repository;
 import org.motechproject.tama.common.domain.TAMAUser;
 import org.motechproject.tama.facility.domain.Clinician;
 import org.motechproject.tama.refdata.domain.Administrator;
+import org.motechproject.tama.refdata.domain.Analyst;
 import org.motechproject.tama.refdata.repository.AllAdministrators;
+import org.motechproject.tama.refdata.repository.AllAnalysts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -17,11 +19,16 @@ public class AllTAMAUsers {
     @Autowired
     AllAdministrators allAdministrators;
 
+    @Autowired
+    AllAnalysts allAnalysts;
+
     public void update(TAMAUser user, String userName) {
         if (user instanceof Clinician) {
             allClinicians.updatePassword((Clinician) user, userName);
         } else if (user instanceof Administrator) {
             allAdministrators.updatePassword((Administrator) user);
+        } else if (user instanceof Analyst) {
+            allAnalysts.updatePassword((Analyst) user);
         }
     }
 
