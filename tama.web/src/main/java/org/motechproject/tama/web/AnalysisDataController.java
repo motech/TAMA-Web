e9251c6264061b6acefdeae45f9687cfc36c9264
Balaji Narain <balajin@thoughtworks.com>
2013-01-28 21:40:30 +0530

    BalajiN | #5 | Adding link to download patient registration details

--- tama.web/src/main/java/org/motechproject/tama/web/AnalysisDataController.java
+++ tama.web/src/main/java/org/motechproject/tama/web/AnalysisDataController.java
@@ -1,6 +1,9 @@
 package org.motechproject.tama.web;
 
+import org.motechproject.tama.reporting.properties.ReportingProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,8 +11,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("analysisData")
 public class AnalysisDataController extends BaseController {
 
+    private CallSummaryController callSummaryController;
+    private ReportingProperties reportingProperties;
+
+    @Autowired
+    public AnalysisDataController(CallSummaryController callSummaryController, ReportingProperties reportingProperties) {
+        this.callSummaryController = callSummaryController;
+        this.reportingProperties = reportingProperties;
+    }
+
     @RequestMapping(method = RequestMethod.GET)
-    public String show() {
-        return "redirect:/callsummary?form";
+    public String show(Model uiModel) {
+        uiModel.addAttribute("reports_url", reportingProperties.reportingURL());
+        callSummaryController.filterLogs(uiModel);
+        return "analysisData/show";
     }
 }
--- tama.web/src/test/java/org/motechproject/tama/web/AnalysisDataControllerTest.java
+++ tama.web/src/test/java/org/motechproject/tama/web/AnalysisDataControllerTest.java
@@ -3,20 +3,36 @@ package org.motechproject.tama.web;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.motechproject.tama.reporting.properties.ReportingProperties;
+import org.springframework.ui.Model;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 import static org.openqa.selenium.support.testing.Assertions.assertEquals;
 
 public class AnalysisDataControllerTest {
+
+    @Mock
+    private CallSummaryController callSummaryController;
+    @Mock
+    private ReportingProperties reportingProperties;
+    @Mock
+    private Model model;
 
     private AnalysisDataController analysisDataController;
 
     @Before
     public void setup() {
-        analysisDataController = new AnalysisDataController();
+        initMocks(this);
+        analysisDataController = new AnalysisDataController(callSummaryController, reportingProperties);
     }
 
     @Test
     public void shouldShowCallLogsFilterAsTheLandingPage() throws Exception {
-        assertEquals("redirect:/callsummary?form", analysisDataController.show());
+        when(reportingProperties.reportingURL()).thenReturn("url");
+        assertEquals("analysisData/show", analysisDataController.show(model));
+        verify(model).addAttribute("reports_url", "url");
     }
 }
