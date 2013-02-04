e7dbb91d29be689af9770f9863c973297c619378
Balaji Narain <balajin@thoughtworks.com>
2013-02-04 15:23:38 +0530

    Shashank,BalajiN | #0 | Adding default values for date fields in date filters

--- tama.web/src/main/java/org/motechproject/tama/web/model/DateFilter.java
+++ tama.web/src/main/java/org/motechproject/tama/web/model/DateFilter.java
@@ -1,10 +1,27 @@
 package org.motechproject.tama.web.model;
 
 import lombok.Data;
-import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.motechproject.tama.common.TAMAConstants;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import static org.motechproject.util.DateUtil.today;
 
 @Data
 public class DateFilter {
-    public DateTime startDate;
-    public DateTime endDate;
+
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(style = "S-", pattern = TAMAConstants.DATE_FORMAT)
+    public LocalDate startDate;
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(style = "S-", pattern = TAMAConstants.DATE_FORMAT)
+    public LocalDate endDate;
+
+    public DateFilter() {
+        startDate = today();
+        endDate = today();
+    }
 }
