74f06aee0566b090ed5d86e486befe4df97345d0
Balaji Narain <balajin@thoughtworks.com>
2013-03-20 18:01:21 +0530

    Shashank,BalajiN | #0 | The Open element div does not have a link

--- tama.web/src/main/webapp/js/list_alerts.js
+++ tama.web/src/main/webapp/js/list_alerts.js
@@ -3,7 +3,7 @@ dojo.addOnLoad(function () {
     dojo.forEach(dojo.query("tr td"), function (element, index) {
 
         if(element.innerHTML.indexOf("Open") >= 0){
-            dojo.addClass(dojo.query("a", element)[0], "blueText");
+            dojo.addClass(element, "blueText");
         }
     });
 });
