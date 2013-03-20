26e14b774b537ed063c165015138c92886aed233
Balaji Narain <balajin@thoughtworks.com>
2013-03-20 16:40:34 +0530

    Shashank, BalajiN | #0 | dojo.query returns an array

--- tama.web/src/main/webapp/js/list_patients.js
+++ tama.web/src/main/webapp/js/list_patients.js
@@ -3,7 +3,7 @@ dojo.addOnLoad(function () {
     dojo.forEach(dojo.query("tr td"), function (element, index) {
 
         if(element.innerHTML.indexOf("Deactivate") >= 0){
-            dojo.addClass(dojo.query("a", element), "greyText");
+            dojo.addClass(dojo.query("a", element)[0], "greyText");
         }
     });
 });
