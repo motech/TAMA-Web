a350a9f4a33b6afc8dbd67e075ac8035ab09f0a2
Balaji Narain <balajin@thoughtworks.com>
2013-04-02 17:48:27 +0530

    Shashank,BalajiN | #86 | Not playing adherence trend feedback as a part of regular fdr flow

--- tama.fourdayrecall/src/main/java/org/motechproject/tama/fourdayrecall/command/WeeklyAdherencePercentage.java
+++ tama.fourdayrecall/src/main/java/org/motechproject/tama/fourdayrecall/command/WeeklyAdherencePercentage.java
@@ -3,11 +3,8 @@ package org.motechproject.tama.fourdayrecall.command;
 import org.motechproject.decisiontree.model.ITreeCommand;
 import org.motechproject.ivr.kookoo.KooKooIVRContext;
 import org.motechproject.tama.fourdayrecall.service.FourDayRecallAdherenceService;
-import org.motechproject.tama.fourdayrecall.service.FourDayRecallDateService;
 import org.motechproject.tama.ivr.TamaIVRMessage;
 import org.motechproject.tama.ivr.context.TAMAIVRContext;
-import org.motechproject.tama.patient.repository.AllPatients;
-import org.motechproject.tama.patient.repository.AllTreatmentAdvices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,17 +14,11 @@ import java.util.List;
 @Component
 public class WeeklyAdherencePercentage implements ITreeCommand {
 
-    private AllPatients allPatients;
-    private AllTreatmentAdvices allTreatmentAdvices;
     private FourDayRecallAdherenceService fourDayRecallAdherenceService;
-    private FourDayRecallDateService fourDayRecallDateService;
 
     @Autowired
-    public WeeklyAdherencePercentage(AllPatients allPatients, AllTreatmentAdvices allTreatmentAdvices, FourDayRecallAdherenceService fourDayRecallAdherenceService, FourDayRecallDateService fourDayRecallDateService) {
-        this.allPatients = allPatients;
-        this.allTreatmentAdvices = allTreatmentAdvices;
+    public WeeklyAdherencePercentage(FourDayRecallAdherenceService fourDayRecallAdherenceService) {
         this.fourDayRecallAdherenceService = fourDayRecallAdherenceService;
-        this.fourDayRecallDateService = fourDayRecallDateService;
     }
 
     @Override
@@ -39,38 +30,13 @@ public class WeeklyAdherencePercentage implements ITreeCommand {
     String[] executeCommand(TAMAIVRContext ivrContext) {
         List<String> messages = new ArrayList<String>();
 
-        String patientDocumentId = ivrContext.patientDocumentId();
-
         final int numDaysMissed = Integer.parseInt(ivrContext.dtmfInput());
         int currentWeekAdherencePercentage = fourDayRecallAdherenceService.adherencePercentageFor(numDaysMissed);
-        boolean falling = fourDayRecallAdherenceService.isAdherenceFalling(numDaysMissed, patientDocumentId);
 
         messages.add(TamaIVRMessage.FDR_YOUR_WEEKLY_ADHERENCE_IS);
         messages.add(TamaIVRMessage.getNumberFilename(currentWeekAdherencePercentage));
         messages.add(TamaIVRMessage.FDR_PERCENT);
 
-        if (!fourDayRecallDateService.isFirstTreatmentWeek(allPatients.get(patientDocumentId), allTreatmentAdvices.currentTreatmentAdvice(patientDocumentId))) {
-            addTrendMessages(messages, currentWeekAdherencePercentage, falling);
-        }
-
         return messages.toArray(new String[messages.size()]);
-    }
-
-    private void addTrendMessages(List<String> messages, int currentWeekAdherencePercentage, boolean falling) {
-        if (currentWeekAdherencePercentage > 90) {
-            messages.add(TamaIVRMessage.M02_04_ADHERENCE_COMMENT_GT95_FALLING);
-        } else if (currentWeekAdherencePercentage > 70) {
-            if (falling) {
-                messages.add(TamaIVRMessage.M02_05_ADHERENCE_COMMENT_70TO90_FALLING);
-            } else {
-                messages.add(TamaIVRMessage.M02_06_ADHERENCE_COMMENT_70TO90_RISING);
-            }
-        } else {
-            if (falling) {
-                messages.add(TamaIVRMessage.M02_07_ADHERENCE_COMMENT_LT70_FALLING);
-            } else {
-                messages.add(TamaIVRMessage.M02_08_ADHERENCE_COMMENT_LT70_RISING);
-            }
-        }
     }
 }
--- tama.fourdayrecall/src/test/java/org/motechproject/tama/fourdayrecall/command/WeeklyAdherencePercentageTest.java
+++ tama.fourdayrecall/src/test/java/org/motechproject/tama/fourdayrecall/command/WeeklyAdherencePercentageTest.java
@@ -6,109 +6,42 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.motechproject.tama.common.NoAdherenceRecordedException;
 import org.motechproject.tama.fourdayrecall.service.FourDayRecallAdherenceService;
-import org.motechproject.tama.fourdayrecall.service.FourDayRecallDateService;
 import org.motechproject.tama.ivr.TAMAIVRContextForTest;
 import org.motechproject.tama.ivr.TamaIVRMessage;
-import org.motechproject.tama.patient.repository.AllPatients;
-import org.motechproject.tama.patient.repository.AllTreatmentAdvices;
 
 import static junit.framework.Assert.assertEquals;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class WeeklyAdherencePercentageTest {
 
     public static final String PATIENT_ID = "patientId";
-    @Mock
-    private AllPatients allPatients;
-    @Mock
-    private AllTreatmentAdvices allTreatmentAdvices;
+
     @Mock
     private FourDayRecallAdherenceService fourDayRecallAdherenceService;
-    @Mock
-    private FourDayRecallDateService fourDayRecallDateService;
+    private TAMAIVRContextForTest ivrContext;
 
     private WeeklyAdherencePercentage weeklyAdherencePercentage;
-    private TAMAIVRContextForTest ivrContext;
 
     @Before
     public void setUp() {
         initMocks(this);
         ivrContext = new TAMAIVRContextForTest().patientDocumentId(PATIENT_ID);
-        weeklyAdherencePercentage = new WeeklyAdherencePercentage(allPatients, allTreatmentAdvices, fourDayRecallAdherenceService, fourDayRecallDateService);
+        weeklyAdherencePercentage = new WeeklyAdherencePercentage(fourDayRecallAdherenceService);
     }
 
     @Test
     public void shouldReturnCorrectMessage_WhenCurrentWeekAdherenceIsGreaterThan90() throws NoAdherenceRecordedException {
         when(fourDayRecallAdherenceService.adherencePercentageFor(anyInt())).thenReturn(100);
-        when(fourDayRecallAdherenceService.getAdherencePercentageForPreviousWeek(PATIENT_ID)).thenReturn(50);
         ivrContext.dtmfInput("0");
 
         String[] audios = weeklyAdherencePercentage.executeCommand(ivrContext);
-        assertEquals(4, audios.length);
+        assertEquals(3, audios.length);
         assertThat(audios[0], is(TamaIVRMessage.FDR_YOUR_WEEKLY_ADHERENCE_IS));
         assertThat(audios[1], is("Num_100"));
         assertThat(audios[2], is(TamaIVRMessage.FDR_PERCENT));
-        assertThat(audios[3], is(TamaIVRMessage.M02_04_ADHERENCE_COMMENT_GT95_FALLING));
-    }
-
-    @Test
-    public void shouldReturnCorrectMessage_WhenCurrentWeekAdherenceIsGreaterThan70_AndFalling() {
-        when(fourDayRecallAdherenceService.adherencePercentageFor(anyInt())).thenReturn(80);
-        when(fourDayRecallAdherenceService.isAdherenceFalling(anyInt(), anyString())).thenReturn(true);
-        ivrContext.dtmfInput("1");
-
-        String[] audios = weeklyAdherencePercentage.executeCommand(ivrContext);
-        assertEquals(4, audios.length);
-        assertThat(audios[0], is(TamaIVRMessage.FDR_YOUR_WEEKLY_ADHERENCE_IS));
-        assertThat(audios[1], is("Num_080"));
-        assertThat(audios[2], is(TamaIVRMessage.FDR_PERCENT));
-        assertThat(audios[3], is(TamaIVRMessage.M02_05_ADHERENCE_COMMENT_70TO90_FALLING));
-    }
-
-    @Test
-    public void shouldReturnCorrectMessage_WhenCurrentWeekAdherenceIsGreaterThan70_AndRising() {
-        when(fourDayRecallAdherenceService.adherencePercentageFor(anyInt())).thenReturn(80);
-        when(fourDayRecallAdherenceService.isAdherenceFalling(anyInt(), anyString())).thenReturn(false);
-        ivrContext.dtmfInput("1");
-
-        String[] audios = weeklyAdherencePercentage.executeCommand(ivrContext);
-        assertEquals(4, audios.length);
-        assertThat(audios[0], is(TamaIVRMessage.FDR_YOUR_WEEKLY_ADHERENCE_IS));
-        assertThat(audios[1], is("Num_080"));
-        assertThat(audios[2], is(TamaIVRMessage.FDR_PERCENT));
-        assertThat(audios[3], is(TamaIVRMessage.M02_06_ADHERENCE_COMMENT_70TO90_RISING));
-    }
-
-    @Test
-    public void shouldReturnCorrectMessage_WhenCurrentWeekAdherenceIsLessThan70_AndFalling() {
-        when(fourDayRecallAdherenceService.adherencePercentageFor(anyInt())).thenReturn(60);
-        when(fourDayRecallAdherenceService.isAdherenceFalling(anyInt(), anyString())).thenReturn(true);
-        ivrContext.dtmfInput("2");
-
-        String[] audios = weeklyAdherencePercentage.executeCommand(ivrContext);
-        assertEquals(4, audios.length);
-        assertThat(audios[0], is(TamaIVRMessage.FDR_YOUR_WEEKLY_ADHERENCE_IS));
-        assertThat(audios[1], is("Num_060"));
-        assertThat(audios[2], is(TamaIVRMessage.FDR_PERCENT));
-        assertThat(audios[3], is(TamaIVRMessage.M02_07_ADHERENCE_COMMENT_LT70_FALLING));
-    }
-
-    @Test
-    public void shouldReturnCorrectMessage_WhenCurrentWeekAdherenceIsLessThan70_AndRising() {
-        when(fourDayRecallAdherenceService.adherencePercentageFor(anyInt())).thenReturn(60);
-        when(fourDayRecallAdherenceService.isAdherenceFalling(anyInt(), anyString())).thenReturn(false);
-        ivrContext.dtmfInput("2");
-
-        String[] audios = weeklyAdherencePercentage.executeCommand(ivrContext);
-        assertEquals(4, audios.length);
-        assertThat(audios[0], is(TamaIVRMessage.FDR_YOUR_WEEKLY_ADHERENCE_IS));
-        assertThat(audios[1], is("Num_060"));
-        assertThat(audios[2], is(TamaIVRMessage.FDR_PERCENT));
-        assertThat(audios[3], is(TamaIVRMessage.M02_08_ADHERENCE_COMMENT_LT70_RISING));
     }
 }
