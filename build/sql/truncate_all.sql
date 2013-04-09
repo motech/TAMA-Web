6214a0e5fc0190fd6bfe1ba79030fa98e9e4b2f0
Balaji Narain <balajin@thoughtworks.com>
2013-04-09 17:29:09 +0530

    Shashank, BalajiN | #0 | Unsing postgres instead of mysql for quartz

--- build/sql/truncate_all.sql
+++ build/sql/truncate_all.sql
@@ -1,13 +1,13 @@
-delete from QRTZ_BLOB_TRIGGERS;
-delete from QRTZ_CALENDARS;
-delete from QRTZ_CRON_TRIGGERS;
-delete from QRTZ_FIRED_TRIGGERS;
-delete from QRTZ_JOB_LISTENERS;
-delete from QRTZ_LOCKS;
-delete from QRTZ_PAUSED_TRIGGER_GRPS;
-delete from QRTZ_SCHEDULER_STATE;
-delete from QRTZ_SIMPLE_TRIGGERS;
-delete from QRTZ_TRIGGERS;
-delete from QRTZ_TRIGGER_LISTENERS;
-delete from QRTZ_JOB_DETAILS;
+delete from qrtz_blob_triggers;
+delete from qrtz_calendars;
+delete from qrtz_cron_triggers;
+delete from qrtz_fired_triggers;
+delete from qrtz_job_listeners;
+delete from qrtz_locks;
+delete from qrtz_paused_trigger_grps;
+delete from qrtz_scheduler_state;
+delete from qrtz_simple_triggers;
+delete from qrtz_triggers;
+delete from qrtz_trigger_listeners;
+delete from qrtz_job_details;
 
