1fb3515ddb19e4cde91ba2e33888022675375a97
Shashank Tomar <shashank004@gmail.com>
2013-04-17 11:27:41 +0530

    Shashank | Changed the color of Open and Closed alerts

--- tama.web/src/main/webapp/js/list_alerts.js
+++ tama.web/src/main/webapp/js/list_alerts.js
@@ -3,7 +3,11 @@ dojo.addOnLoad(function () {
     dojo.forEach(dojo.query("tr td"), function (element, index) {
 
         if(element.innerHTML.indexOf("Open") >= 0){
-            dojo.addClass(element, "blueText");
+            dojo.addClass(element, "redText");
+        }
+
+        if(element.innerHTML.indexOf("Closed") >= 0){
+            dojo.addClass(element, "greenText");
         }
     });
 });
