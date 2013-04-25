82abbd6982735c42437b782d6b2fe0323bc6ace4
Balaji Narain <balajin@thoughtworks.com>
2013-04-25 11:59:34 +0530

    BalajiN | #0 | Using HttpSessionEventPublisher instead of listener to correctly handle session timeouts

--- tama.web/src/main/java/org/motechproject/tama/security/TamaSessionListener.java
+++ tama.web/src/main/java/org/motechproject/tama/security/TamaSessionListener.java
@@ -1,14 +1,14 @@
 package org.motechproject.tama.security;
 
 import org.motechproject.tama.security.repository.AllTAMAEvents;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
-import javax.servlet.http.HttpSessionListener;
 
-public class TamaSessionListener implements HttpSessionListener {
+public class TamaSessionListener extends HttpSessionEventPublisher {
 
     private final String sourceAddress = "Session Timeout";
 
@@ -19,14 +19,17 @@ public class TamaSessionListener implements HttpSessionListener {
 
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
-        final HttpSession session = se.getSession();
+        HttpSession session = se.getSession();
         if (session == null) return;
         Object userObject = session.getAttribute(LoginSuccessHandler.LOGGED_IN_USER);
-        if (userObject instanceof AuthenticatedUser){
-            AuthenticatedUser authenticatedUser = (AuthenticatedUser)userObject;
+        if (userObject instanceof AuthenticatedUser) {
+            AuthenticatedUser authenticatedUser = (AuthenticatedUser) userObject;
             WebApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(session.getServletContext());
-            AllTAMAEvents allTAMAEvents = (AllTAMAEvents)ctx.getBean("allTAMAEvents");
+            AllTAMAEvents allTAMAEvents = (AllTAMAEvents) ctx.getBean("allTAMAEvents");
             allTAMAEvents.newLogoutEvent(authenticatedUser.getTAMAUser().getUsername(), sourceAddress, session.getId());
+            allTAMAEvents = null;
+            userObject = null;
         }
+        super.sessionDestroyed(se);
     }
 }
