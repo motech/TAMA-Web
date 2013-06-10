2b601e9b4ec2e0eccaf34802d5679a2d98176cc7
Vikram Rao <vikramra@thoughtworks.com>
2013-06-10 12:08:00 +0530

    Vikram | date format issue weekly report

--- tama.fourdayrecall/src/main/java/org/motechproject/tama/fourdayrecall/reporting/WeeklyAdherenceMapper.java
+++ tama.fourdayrecall/src/main/java/org/motechproject/tama/fourdayrecall/reporting/WeeklyAdherenceMapper.java
@@ -17,8 +17,6 @@ import org.motechproject.tama.reports.contract.WeeklyAdherenceLogRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
-
 @Component
 public class WeeklyAdherenceMapper {
 
@@ -47,26 +45,13 @@ public class WeeklyAdherenceMapper {
         WeeklyAdherenceLogRequest weeklyAdherenceLogRequest = new WeeklyAdherenceLogRequest();
         weeklyAdherenceLogRequest.setPatientId(weeklyAdherenceLog.getPatientId());
         weeklyAdherenceLogRequest.setClinicName(patient.getClinic().getName());
-        weeklyAdherenceLogRequest.setArtStartDate(formatDate("dd/MM/yyyy h:mm aa",patient.getActivationDate()).toDate());
+        weeklyAdherenceLogRequest.setArtStartDate(patient.getActivationDate().toDate());
         weeklyAdherenceLogRequest.setTreatmentAdviceId(regimen.getDisplayName());
-        weeklyAdherenceLogRequest.setStartDate(formatDate("dd/MM/yyyy h:mm aa",treatmentAdvice.getStartDate()).toDate());
-        weeklyAdherenceLogRequest.setWeekStartDate(formatDate("dd/MM/yyyy h:mm aa",weeklyAdherenceLog.getWeekStartDate().toDate()).toDate());
-        weeklyAdherenceLogRequest.setAdherenceLoggedDate(formatDate("dd/MM/yyyy h:mm aa",weeklyAdherenceLog.getLogDate()).toDate());
+        weeklyAdherenceLogRequest.setStartDate(treatmentAdvice.getStartDate());
+        weeklyAdherenceLogRequest.setWeekStartDate(weeklyAdherenceLog.getWeekStartDate().toDate());
+        weeklyAdherenceLogRequest.setAdherenceLoggedDate(weeklyAdherenceLog.getLogDate().toDate());
         weeklyAdherenceLogRequest.setNumberOfDaysMissed(weeklyAdherenceLog.getNumberOfDaysMissed());
         return weeklyAdherenceLogRequest;
     }
 
-    private DateTime formatDate(String dateFormat,DateTime dateTime)
-    {
-        DateTimeFormatter format = DateTimeFormat.forPattern(dateFormat);
-        DateTime time = format.parseDateTime(dateTime.toString());
-        return time;
-    }
-    private DateTime formatDate(String dateFormat,Date date)
-    {
-        DateTimeFormatter format = DateTimeFormat.forPattern(dateFormat);
-        DateTime time = format.parseDateTime(date.toString());
-        return time;
-
-    }
 }
