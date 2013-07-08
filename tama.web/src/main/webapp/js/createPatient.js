af3f2d986ae84af9764435b750dac5f1942666dc
Vikram Rao <vikramra@thoughtworks.com>
2013-07-08 16:46:37 +0530

    Vikram | fix for unique phone number in weekly patient case

--- tama.web/src/main/webapp/js/createPatient.js
+++ tama.web/src/main/webapp/js/createPatient.js
@@ -7,6 +7,7 @@ function warningWhenStartingOnFourDayRecall(event) {
     dialog.execute = function () {
         dojo.byId("patient").submit();
     };
+
     var isCallPreferenceDailyPillReminder = dojo.byId('dailyReminderCall').checked;
     if (!isCallPreferenceDailyPillReminder) {
         if(Spring.validateAll()){
@@ -30,6 +31,44 @@ function showPatientPreferences() {
     hideElement([nextToPatientPreferencesButton]);
     showElement([preferencesPanel, submitButtonDiv]);
 }
+
+  function validateMobileNumberUniqueness()
+  {
+        // get the form values
+
+        var mobileNumber = dijit.byId('_mobilePhoneNumber_id').value;
+        var codeExecuted = false;
+
+        if(codeExecuted == false)
+        {
+
+        AjaxCall.get({
+        url: "/tama/patients/validateMobileNumberUniqueness.json",
+        content: {mobileNumber:mobileNumber},
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        sync: true,
+        load: handleResults
+
+        });
+        }
+  }
+
+  function handleResults(response,ioArgs)
+  {
+        if(response.status == "FAIL")
+        {
+        if(Spring.validateAll()){
+        codeExecuted=true;
+        alert("Warning !   The phone number entered for the patient is already in use");
+        }
+
+        }
+        else
+        {
+        console.log("PASSED");
+        }
+  }
 
 function fillAllDetails() {
     hideElement([fillAllButton]);
@@ -67,6 +106,7 @@ dojo.addOnLoad(function() {
     //dojo.connect(dojo.byId('proceed'), 'onclick', warningWhenStartingOnFourDayRecall);
     dojo.connect(dojo.byId('savePatient'), 'onclick', warningWhenStartingOnFourDayRecall);
     dojo.connect(dojo.byId('activatePatient'), 'onclick', warningWhenStartingOnFourDayRecall);
+
 });
 
 function encodePk(){
